c7729f9ce8e5e8988af1b7183ffe634bd9af3cf9
Kareem <soratka1@jhuapl.edu>
2019-07-26 15:09:54 -0400

    Continuing solver testing

--- src/base/mgmres.F90
+++ src/base/mgmres.F90
@@ -10,73 +10,6 @@ module mgmres
 
 subroutine ax_cr ( n, nz_num, ia, ja, a, x, w )
 
-!*****************************************************************************80
-!
-!! AX_CR computes A*x for a matrix stored in sparse compressed row form.
-!
-!  Discussion:
-!
-!    The Sparse Compressed Row storage format is used.
-!
-!    The matrix A is assumed to be sparse.  To save on storage, only
-!    the nonzero entries of A are stored.  The vector JA stores the
-!    column index of the nonzero value.  The nonzero values are sorted
-!    by row, and the compressed row vector IA then has the property that
-!    the entries in A and JA that correspond to row I occur in indices
-!    IA[I] through IA[I+1]-1.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    17 July 2007
-!
-!  Author:
-!
-!    Original C version by Lili Ju.
-!    FORTRAN90 version by John Burkardt.
-!
-!  Reference:
-!
-!    Richard Barrett, Michael Berry, Tony Chan, James Demmel,
-!    June Donato, Jack Dongarra, Victor Eijkhout, Roidan Pozo,
-!    Charles Romine, Henk van der Vorst,
-!    Templates for the Solution of Linear Systems:
-!    Building Blocks for Iterative Methods,
-!    SIAM, 1994.
-!    ISBN: 0898714710,
-!    LC: QA297.8.T45.
-!
-!    Tim Kelley,
-!    Iterative Methods for Linear and Nonlinear Equations,
-!    SIAM, 2004,
-!    ISBN: 0898713528,
-!    LC: QA297.8.K45.
-!
-!    Yousef Saad,
-!    Iterative Methods for Sparse Linear Systems,
-!    Second Edition,
-!    SIAM, 2003,
-!    ISBN: 0898715342,
-!    LC: QA188.S17.
-!
-!  Parameters:
-!
-!    Input, integer ( kind = 4 ) N, the order of the system.
-!
-!    Input, integer ( kind = 4 ) NZ_NUM, the number of nonzeros.
-!
-!    Input, integer ( kind = 4 ) IA(N+1), JA(NZ_NUM), the row and column
-!    indices of the matrix values.  The row vector has been compressed.
-!
-!    Input, real ( kind = 8 ) A(NZ_NUM), the matrix values.
-!
-!    Input, real ( kind = 8 ) X(N), the vector to be multiplied by A.
-!
-!    Output, real ( kind = 8 ) W(N), the value of A*X.
-!
   implicit none
 
   integer ( kind = 4 ) n
@@ -105,51 +38,6 @@ end
 
 subroutine diagonal_pointer_cr ( n, nz_num, ia, ja, ua )
 
-!*****************************************************************************80
-!
-!! DIAGONAL_POINTER_CR finds diagonal entries in a sparse compressed row matrix.
-!
-!  Discussion:
-!
-!    The matrix A is assumed to be stored in compressed row format.  Only
-!    the nonzero entries of A are stored.  The vector JA stores the
-!    column index of the nonzero value.  The nonzero values are sorted
-!    by row, and the compressed row vector IA then has the property that
-!    the entries in A and JA that correspond to row I occur in indices
-!    IA[I] through IA[I+1]-1.
-!
-!    The array UA can be used to locate the diagonal elements of the matrix.
-!
-!    It is assumed that every row of the matrix includes a diagonal element,
-!    and that the elements of each row have been ascending sorted.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    18 July 2007
-!
-!  Author:
-!
-!    Original C version by Lili Ju.
-!    FORTRAN90 version by John Burkardt.
-!
-!  Parameters:
-!
-!    Input, integer ( kind = 4 ) N, the order of the system.
-!
-!    Input, integer ( kind = 4 ) NZ_NUM, the number of nonzeros.
-!
-!    Input, integer ( kind = 4 ) IA(N+1), JA(NZ_NUM), the row and column
-!    indices of the matrix values.  The row vector has been compressed.
-!    On output, the order of the entries of JA may have changed because of
-!    the sorting.
-!
-!    Output, integer ( kind = 4 ) UA(N), the index of the diagonal element
-!    of each row.
-!
   implicit none
 
   integer ( kind = 4 ) n
@@ -175,48 +63,6 @@ subroutine diagonal_pointer_cr ( n, nz_num, ia, ja, ua )
 end
 subroutine ilu_cr ( n, nz_num, ia, ja, a, ua, l )
 
-!*****************************************************************************80
-!
-!! ILU_CR computes the incomplete LU factorization of a matrix.
-!
-!  Discussion:
-!
-!    The matrix A is assumed to be stored in compressed row format.  Only
-!    the nonzero entries of A are stored.  The vector JA stores the
-!    column index of the nonzero value.  The nonzero values are sorted
-!    by row, and the compressed row vector IA then has the property that
-!    the entries in A and JA that correspond to row I occur in indices
-!    IA(I) through IA(I+1)-1.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    27 July 2007
-!
-!  Author:
-!
-!    Original C version by Lili Ju.
-!    FORTRAN90 version by John Burkardt.
-!
-!  Parameters:
-!
-!    Input, integer ( kind = 4 ) N, the order of the system.
-!
-!    Input, integer ( kind = 4 ) NZ_NUM, the number of nonzeros.
-!
-!    Input, integer ( kind = 4 ) IA(N+1), JA(NZ_NUM), the row and column
-!    indices of the matrix values.  The row vector has been compressed.
-!
-!    Input, real ( kind = 8 ) A(NZ_NUM), the matrix values.
-!
-!    Input, integer ( kind = 4 ) UA(N), the index of the diagonal element
-!    of each row.
-!
-!    Output, real ( kind = 8 ) L(NZ_NUM), the ILU factorization of A.
-!
   implicit none
 
   integer ( kind = 4 ) n
@@ -371,55 +217,121 @@ end
 !         L(ua(1:n)) = 1.0/L(ua(1:n))
 !     end subroutine ilu_cr
 
-!---------------
-    subroutine lus_cr(n,nz_num,ia,ja,L,ua,R,Z)
-        integer, intent(in) :: n,nz_num
-        integer, intent(in) :: ia(n+1),ja(nz_num),ua(n)
-        real(rp), intent(inout) :: L(nz_num)
-        real(rp), intent(in) :: R(n)
-        real(rp), intent(out) :: Z(n)
+subroutine lus_cr ( n, nz_num, ia, ja, l, ua, r, z )
 
-        integer :: i,j
-        real(rp), dimension(n) :: W
+  implicit none
 
-        !Copy in
-        W(1:n) = R(1:n)
+  integer ( kind = 4 ) n
+  integer ( kind = 4 ) nz_num
 
-        !Solve L*W=W, L unit lower triangular
-        !TODO: Add OMP bindings here
-        do i=2,n
-            do j=ia(i),ua(i)-1
-                W(i) = W(i) - L(j)*W(ja(j))
-            enddo
-        enddo
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) ia(n+1)
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ) ja(nz_num)
+  real ( kind = 8 ) l(nz_num)
+  real ( kind = 8 ) r(n)
+  integer ( kind = 4 ) ua(n)
+  real ( kind = 8 ) w(n)
+  real ( kind = 8 ) z(n)
+!
+!  Copy R in.
+!
+  w(1:n) = r(1:n)
+!
+!  Solve L * w = w where L is unit lower triangular.
+!
+  do i = 2, n
+    do j = ia(i), ua(i) - 1
+      w(i) = w(i) - l(j) * w(ja(j))
+    end do
+  end do
+!
+!  Solve U * w = w, where U is upper triangular.
+!
+  do i = n, 1, -1
+    do j = ua(i) + 1, ia(i+1) - 1
+      w(i) = w(i) - l(j) * w(ja(j))
+    end do
+    w(i) = w(i) / l(ua(i))
+  end do
+!
+!  Copy Z out.
+!
+  z(1:n) = w(1:n)
 
-        !Solve U*W=W, U upper triangular
-        do i=n,1,-1
-            do j=ua(i)+1,ia(i+1)-1
-                W(i) = W(i) - L(j)*W(ja(j))
-            enddo
-            W(i) = W(i)/L(ua(i))
-        enddo
+  return
+end
 
-        !Copy out
-        Z(1:n) = W(1:n)
+! !---------------
+!     subroutine lus_cr(n,nz_num,ia,ja,L,ua,R,Z)
+!         integer, intent(in) :: n,nz_num
+!         integer, intent(in) :: ia(n+1),ja(nz_num),ua(n)
+!         real(rp), intent(inout) :: L(nz_num)
+!         real(rp), intent(in) :: R(n)
+!         real(rp), intent(out) :: Z(n)
 
-    end subroutine lus_cr
+!         integer :: i,j
+!         real(rp), dimension(n) :: W
 
-!---------------
-    !Givens rotation
-    subroutine mult_givens(c,s,k,g)
-        real(rp), intent(in) :: c,s !cos/sin of rotation
-        integer, intent(in) :: k
-        real(rp), intent(inout) :: g(1:k+1)
+!         !Copy in
+!         W(1:n) = R(1:n)
 
-        real(rp) :: g1,g2
-        g1 = c*g(k) - s*g(k+1)
-        g2 = s*g(k) - c*g(k+1)
+!         !Solve L*W=W, L unit lower triangular
+!         !TODO: Add OMP bindings here
+!         do i=2,n
+!             do j=ia(i),ua(i)-1
+!                 W(i) = W(i) - L(j)*W(ja(j))
+!             enddo
+!         enddo
 
-        g(k  ) = g1
-        g(k+1) = g2
-    end subroutine mult_givens
+!         !Solve U*W=W, U upper triangular
+!         do i=n,1,-1
+!             do j=ua(i)+1,ia(i+1)-1
+!                 W(i) = W(i) - L(j)*W(ja(j))
+!             enddo
+!             W(i) = W(i)/L(ua(i))
+!         enddo
+
+!         !Copy out
+!         Z(1:n) = W(1:n)
+
+!     end subroutine lus_cr
+
+subroutine mult_givens ( c, s, k, g )
+
+  implicit none
+
+  integer ( kind = 4 ) k
+
+  real ( kind = 8 ) c
+  real ( kind = 8 ) g(1:k+1)
+  real ( kind = 8 ) g1
+  real ( kind = 8 ) g2
+  real ( kind = 8 ) s
+
+  g1 = c * g(k) - s * g(k+1)
+  g2 = s * g(k) + c * g(k+1)
+
+  g(k)   = g1
+  g(k+1) = g2
+
+  return
+end
+
+! !---------------
+!     !Givens rotation
+!     subroutine mult_givens(c,s,k,g)
+!         real(rp), intent(in) :: c,s !cos/sin of rotation
+!         integer, intent(in) :: k
+!         real(rp), intent(inout) :: g(1:k+1)
+
+!         real(rp) :: g1,g2
+!         g1 = c*g(k) - s*g(k+1)
+!         g2 = s*g(k) - c*g(k+1)
+
+!         g(k  ) = g1
+!         g(k+1) = g2
+!     end subroutine mult_givens
 
 ! !---------------
 !     !Preconditioned restarted GMRES
