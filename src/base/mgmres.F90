9c4921fab1dd9b252665924a7abedeaaa9b8a348
Kareem <soratka1@jhuapl.edu>
2019-07-26 13:53:56 -0400

    Testing new solver, givens rotation

--- src/base/mgmres.F90
+++ src/base/mgmres.F90
@@ -1,8 +1,9 @@
 module mgmres
 
-implicit none
+    use kdefs
+    implicit none
 
-contains
+    contains
 
 subroutine atx_cr ( n, nz_num, ia, ja, a, x, w )
 
@@ -903,83 +904,23 @@ subroutine mgmres_st ( n, nz_num, ia, ja, a, x, rhs, itr_max, mr, tol_abs, &
 
   return
 end
-subroutine mult_givens ( c, s, k, g )
 
-!*****************************************************************************80
-!
-!! MULT_GIVENS applies a Givens rotation to two successive entries of a vector.
-!
-!  Discussion:
-!
-!    In order to make it easier to compare this code with the Original C,
-!    the vector indexing is 0-based.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    08 August 2006
-!
-!  Author:
-!
-!    Original C version by Lili Ju.
-!    FORTRAN90 version by John Burkardt.
-!
-!  Reference:
-!
-!    Richard Barrett, Michael Berry, Tony Chan, James Demmel,
-!    June Donato, Jack Dongarra, Victor Eijkhout, Roidan Pozo,
-!    Charles Romine, Henk van der Vorst,
-!    Templates for the Solution of Linear Systems:
-!    Building Blocks for Iterative Methods,
-!    SIAM, 1994.
-!    ISBN: 0898714710,
-!    LC: QA297.8.T45.
-!
-!    Tim Kelley,
-!    Iterative Methods for Linear and Nonlinear Equations,
-!    SIAM, 2004,
-!    ISBN: 0898713528,
-!    LC: QA297.8.K45.
-!
-!    Yousef Saad,
-!    Iterative Methods for Sparse Linear Systems,
-!    Second Edition,
-!    SIAM, 2003,
-!    ISBN: 0898715342,
-!    LC: QA188.S17.
-!
-!  Parameters:
-!
-!    Input, real ( kind = 8 ) C, S, the cosine and sine of a Givens
-!    rotation.
-!
-!    Input, integer ( kind = 4 ) K, indicates the location of the first
-!    vector entry.
-!
-!    Input/output, real ( kind = 8 ) G(1:K+1), the vector to be modified.
-!    On output, the Givens rotation has been applied to entries G(K) and G(K+1).
-!
-  implicit none
+!---------------
+    !Givens rotation
+    subroutine mult_givens(c,s,k,g)
+        real(rp), intent(in) :: c,s !cos/sin of rotation
+        integer, intent(in) :: k
+        real(rp), intent(inout) :: g(1:k+1)
 
-  integer ( kind = 4 ) k
+        real(rp) :: g1,g2
+        g1 = c*g(k) - s*g(k+1)
+        g2 = s*g(k) - c*g(k+1)
 
-  real ( kind = 8 ) c
-  real ( kind = 8 ) g(1:k+1)
-  real ( kind = 8 ) g1
-  real ( kind = 8 ) g2
-  real ( kind = 8 ) s
+        g(k  ) = g1
+        g(k+1) = g2
+    end subroutine mult_givens
 
-  g1 = c * g(k) - s * g(k+1)
-  g2 = s * g(k) + c * g(k+1)
 
-  g(k)   = g1
-  g(k+1) = g2
-
-  return
-end
 subroutine pmgmres_ilu_cr ( n, nz_num, ia, ja, a, x, rhs, itr_max, mr, &
   tol_abs, tol_rel )
 
