4365e21614f7f46671e26a279821d13630d9316b
vmerkin <slava.merkin@jhuapl.edu>
2019-10-25 13:46:42 -0400

    Adding doStarlight and doMR options for 1) adding background quadratically (e.g., Ridley 2004); 2) George Khazanov's multiple reflection correction.

--- src/base/types/mixtypes.F90
+++ src/base/types/mixtypes.F90
@@ -19,7 +19,9 @@ module mixtypes
      real(rp) :: ped0
      logical  :: const_sigma
      logical  :: doRamp
-     logical  :: doChill     
+     logical  :: doChill
+     logical  :: doStarlight
+     logical  :: doMR
      logical  :: apply_cap 
 
      ! solver
@@ -85,7 +87,7 @@ module mixtypes
   type mixConductance_T
     integer :: euv_model_type, et_model_type
     real(rp) :: alpha, beta, R, F107,pedmin,hallmin,sigma_ratio,ped0
-    logical :: const_sigma, doRamp, doChill, apply_cap
+    logical :: const_sigma, doRamp, doChill, doStarlight, apply_cap, doMR
 
     ! auxilary variables
     real(rp) :: PI2, ang65, ang100, pref, href, shall
