32ab1029760eae286e1537e4258454573ac8f3e9
vmerkin <slava.merkin@jhuapl.edu>
2019-10-30 12:07:31 -0400

    Adding the rcm_mix interface to the previous commit.

---
+++ src/voltron/remixInterfaces/rcm_mix_interface.F90
@@ -0,0 +1,47 @@
+module rcm_mix_interface
+  use mixdefs
+  use mixgeom
+  use volttypes
+  use rcm_mhd_interfaces
+  
+  implicit none
+
+  type(mixGrid_T), private :: rcmG
+  type(Map_T), private :: rcmMap  
+
+contains
+
+  subroutine init_rcm_mix(rcmApp)
+    type(rcm_mhd_T),intent(in) :: rcmApp
+    real(rp), dimension(:,:), allocatable :: rcmp, rcmt ! remix-style 2-D arrays to hold the RCM grid
+    integer :: i, j, Np, Nt
+
+    Np = size(rcmApp%glong)
+    Nt = size(rcmApp%gcolat)
+
+    if (.not.allocated(rcmp)) allocate(rcmp(Np,Nt))
+    if (.not.allocated(rcmt)) allocate(rcmt(Np,Nt))
+
+    ! constract the 2-D grid
+    do j=1,Np
+       rcmt(j,:) = rcmApp%gcolat
+    enddo
+
+    do i=1,Nt
+       rcmp(:,i) = rcmApp%glong
+    enddo
+
+    ! call remix grid constructor
+    call init_grid_fromTP(rcmG,rcmt,rcmp,SOLVER_GRID=.false.)
+  end subroutine init_rcm_mix
+
+  subroutine map_rcm_mix(voltApp,rcmPot)
+    type(voltApp_T), intent(in) :: voltApp 
+    real(rp), dimension(:,:), allocatable, intent(inout) :: rcmPot
+
+    ! do mapping here since in geo the RCM grid will be moving
+    call mix_set_map(voltApp%remixApp%ion(NORTH)%G,rcmG,rcmMap)
+    call mix_map_grids(rcmMap,voltApp%remixApp%ion(NORTH)%St%Vars(:,:,POT),rcmPot)
+  end subroutine map_rcm_mix
+  
+end module  rcm_mix_interface
