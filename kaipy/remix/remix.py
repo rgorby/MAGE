43fe2c6d1eca1302b54a2ff81c6e0c37468fdd56
vmerkin <slava.merkin@jhuapl.edu>
2019-12-24 06:23:31 -0500

    Cleaned up remix_plot. It's working but more changes coming.

--- kaipy/remix/remix.py
+++ kaipy/remix/remix.py
@@ -1,290 +1,290 @@
 import h5py
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
 
 def get_data(h5file,step):
-    import numpy
-    from numpy import array
-
-    with h5py.File(h5file,'r') as f:
-        ion = {}
-        ion['X'] = f['X'][:]
-        ion['Y'] = f['Y'][:]
-        for h in f['Step#%d'%step].keys():
-            ion[h] = f['Step#%d'%step][h][:]
-        
-    return ion
-
+	with h5py.File(h5file,'r') as f:
+		ion = {}
+		ion['X'] = f['X'][:]
+		ion['Y'] = f['Y'][:]
+		for h in f['Step#%d'%step].keys():
+			ion[h] = f['Step#%d'%step][h][:]
+		
+	return ion
 
 def plot(theta,r,variables,varname,
-         ncontours=51,
-         nticks=11,
-         addlabels={}):
-    from pylab import pi as pi
-    from pylab import cm
-    from pylab import sin,contour,contourf,colorbar,rgrids,thetagrids,axis,text,linspace,arange,pcolormesh
+		 ncontours=51,
+		 nticks=11,
+		 addlabels={},
+		 ax=None,
+		 x=None,
+		 y=None):
 
-    # List all possible variable names here, add more from the input parameter if needed
-    cblabels = {'potential' : r'Potential, kV',
-                'current'   : r'Current density, $\mu\mathrm{A/m^2}$',
-                'sigmap'    : r'Pedersen conductance, S',
-                'sigmah'    : r'Hall conductance, S',
-                'energy'    : r'Energy, keV',
-                'flux'      : r'Flux, $\mathrm{1/cm^2s}$',
-                'ephi'      : r'$E_\phi$,  mV/m',
-                'etheta'    : r'$E_\theta$, mV/m',
-                'efield'    : r'|E|, mV/m',
-                'joule'     : r'Joule heating, mW/m$^2$',
-                'jped'      : r'Pedersen current, $\mu\mathrm{A/m^2}$',
-                'magpert'   : r'Magnetic perturbation, nT',
-                }
-    cblabels.update(addlabels)
+	# List all possible variable names here, add more from the input parameter if needed
+	cblabels = {'potential' : r'Potential [kV]',
+				'current'   : r'Current density [$\mu\mathrm{A/m^2}$]',
+				'sigmap'    : r'Pedersen conductance [S]',
+				'sigmah'    : r'Hall conductance [S]',
+				'energy'    : r'Energy [keV]',
+				'flux'      : r'Flux [$\mathrm{1/cm^2s}$]',
+				'ephi'      : r'$E_\phi$ [mV/m]',
+				'etheta'    : r'$E_\theta$ [mV/m]',
+				'efield'    : r'|E| [mV/m]',
+				'joule'     : r'Joule heating [mW/m$^2$]',
+				'jped'      : r'Pedersen current [$\mu\mathrm{A/m^2}$]',
+				'magpert'   : r'Magnetic perturbation [nT]',
+				}
+	cblabels.update(addlabels)
 
-    # if limits are given use them, if not use the variables min/max values
-    if ('min' in variables[varname]):
-        lower = variables[varname]['min']
-    else:
-        lower = variables[varname]['data'].min()
+	# if limits are given use them, if not use the variables min/max values
+	if ('min' in variables[varname]):
+		lower = variables[varname]['min']
+	else:
+		lower = variables[varname]['data'].min()
 
-    if ('max' in variables[varname]):
-        upper = variables[varname]['max']
-    else:
-        upper = variables[varname]['data'].max()
+	if ('max' in variables[varname]):
+		upper = variables[varname]['max']
+	else:
+		upper = variables[varname]['data'].max()
 
+	# define number format string for min/max labels
+	if varname != 'flux': 
+		if varname == 'jped':
+			format_str = '%.2f'
+		else:
+			format_str = '%.1f'
+	else:
+		format_str = '%.1e'
 
-# Leaving this here for possible later use of pcolor instead of contours
-    # pcolor(-y,x,psi_n*1.e-3) 
-    # cb=colorbar()
-    # cb.set_label('Potential, kV')
-    # pcolor(-y,x,psi_n*1.e-3) 
-    # axes(polar=True)
+	# define red/blue colortable for potential and current
+	if (varname == 'potential') or (varname == 'current'):
+		cmap=cm.RdBu_r
+	elif (varname == 'velocity'): 
+		cmap=cm.YlOrRd
+	elif (varname == 'efield'): 
+		cmap=None #cm.RdBu_r#None # cm.hsv
+	elif (varname == 'magpert'): 
+		cmap = cm.YlOrRd
+	else:
+		cmap=None # default is used
+	
+	# DEFINE GRID LINES AND LABELS
+	circle_list = [10,20,30,40]
+	circles = np.sin(np.array(circle_list)*np.pi/180.)
 
-# define number format string for min/max labels
-    if varname != 'flux': 
-        if varname == 'jped':
-            format_str = '%.2f'
-        else:
-            format_str = '%.1f'
-    else:
-        format_str = '%.1e'
+	# convert to string and add degree symbol
+	lbls = [str(elem)+u'\xb0' for elem in circle_list] 
+	
+	hour_labels = ['06','12','18','00']
 
-# define red/blue colortable for potential and current
-    if (varname == 'potential') or (varname == 'current'):
-        cmap=cm.RdBu_r
-    elif (varname == 'velocity'): 
-        cmap=cm.YlOrRd
-    elif (varname == 'efield'): 
-        cmap=None #cm.RdBu_r#None # cm.hsv
-    elif (varname == 'magpert'): 
-        cmap = cm.YlOrRd
-    else:
-        cmap=None # default is used
-    
-# DEFINE GRID LINES AND LABELS
-# it's a list of circles in degrees
-    circle_list = [10,20,30,40] 
-# circles in radians
-    circles = [sin(elem*pi/180.) for elem in circle_list] 
-# convert to string and add degree symbol
-    lbls = [str(elem)+u'\xb0' for elem in circle_list] 
-    # latex string for the same
-    # ['10$^\circ$','20$^\circ$','30$^\circ$','40$^\circ$']
+	contours = np.linspace(lower,upper,ncontours)
+	ticks = np.linspace(lower,upper,nticks)
 
-    hour_labels = ['06','12','18','00']#{'north':['06','12','18','00'],'south':['18','12','06','00']}
+	variable = variables[varname]['data']
 
-    contours = linspace(lower,upper,ncontours)
-    ticks = linspace(lower,upper,nticks)
+	p=ax.pcolormesh(theta+np.pi/2.,r,variable,cmap=cmap,vmin=lower,vmax=upper)
+	cb=plt.colorbar(p,ax=ax,pad=0.1,ticks=ticks,shrink=0.85)
+	cb.set_label(cblabels[varname])
 
-    variable = variables[varname]['data']
-#    contourf(theta+pi/2.,r,variable,contours,extend='both',cmap=cmap)
-    pcolormesh(theta+pi/2.,r,variable)
-    cb=colorbar(pad=0.075,ticks=ticks,shrink=0.85)
-    cb.set_label(cblabels[varname])
-#    contour(theta+pi/2.,r,variable,contours,cmap=cmap)
-    
-#    from pylab import asarray 
-#    if (varname == 'potential'): contour(theta+pi/2.,r,variables['potential']['data'],21,colors='black')
-    import matplotlib
-#    matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
-    # if (varname == 'efield' or varname == 'velocity' or varname =='joule'): 
-    #     contour(theta+pi/2.,r,variables['potential']['data'][:,2:-1],21,colors='black')
+	lines, labels = plt.rgrids(circles,lbls,fontsize=8)
+	lines, labels = plt.thetagrids((0.,90.,180.,270.),hour_labels)
+	ax.axis([0,2*np.pi,0,r.max()],'tight')
+	ax.text(-75.*np.pi/180.,1.2*r.max(),('min: '+format_str+'\nmax: ' +format_str) % 
+		  (variable.min() ,variable.max()))
+	ax.grid(True)
+
+	if varname=='current': 
+		# use this trick to define contour levels.
+		# if you just use number of levels as an argument for contour,
+		# matplotlib plots a bad contour near pole (apparently a bug)
+		potmax = np.max(abs(variables['potential']['data']))
+		levels = np.linspace(-potmax,potmax,16)
+
+		# this trick seems to avoid problems with contours across periodic boundary (noon)
+		xc = 0.25*(x[:-1,:-1]+x[1:,:-1]+x[:-1,1:]+x[1:,1:])
+		yc = 0.25*(y[:-1,:-1]+y[1:,:-1]+y[:-1,1:]+y[1:,1:])		
+
+		fig = plt.gcf()
+		new_axis = fig.add_axes(ax.get_position(), frameon = False)
+#		new_axis.plot()
+		new_axis.set_axis_off()
+		new_axis.contour(-yc,xc,variables['potential']['data'],levels=levels,colors='black',linewidths=0.5)
+
+#		ax.contour(tc+np.pi/2.,rc,tmp,levels=levels,colors='black',linewidths=0.5)
+	
+	# mpl.rcParams['contour.negative_linestyle'] = 'solid'
+	# if (varname == 'efield' or varname == 'velocity' or varname =='joule'): 
+	#     contour(theta+pi/2.,r,variables['potential']['data'][:,2:-1],21,colors='black')
 #                                      arange(variables['potential']['min'],variables['potential']['max'],21.),colors='purple')
 
-#    if (varname == 'current'): 
-#        contour(theta+pi/2.,r,variables['potential']['data'],21,colors='black')
-
-    lines, labels = rgrids(circles,lbls)
-    lines, labels = thetagrids((0.,90.,180.,270.),hour_labels)
-    axis([0,2*pi,0,r.max()],'tight')
-    text(-75.*pi/180.,1.2*r.max(),('min: '+format_str+'\nmax: ' +format_str) % 
-          (variable.min() ,variable.max()))
-
-    
 
 # The function is deprecated as of 10-25-10 and is left here only for
 # backward compatibility. Use get_time_series for extracting cpcp and
 # other quantities as funcitons of time.
 def get_cpcp_time_series(directory):
-    from pylab import sort
-    import glob,datetime
+	from pylab import sort
+	import glob,datetime
 
-    files = glob.glob(directory+'*_mix_*.hdf')
+	files = glob.glob(directory+'*_mix_*.hdf')
 
-    time   = []
-    cpcp_n = []
-    cpcp_s = []
-    for file in sort(files):
-        print(file)
-        (simtime,
-         x,y,
-         psi_n,psi_s,
-         fac_n,fac_s,
-         sigmap_n,sigmap_s,
-         sigmah_n,sigmah_s,
-         energy_n,energy_s,
-         flux_n,flux_s) = get_data(file)
-    
-        cpcp_n.append(psi_n.max()-psi_n.min())
-        cpcp_s.append(psi_s.max()-psi_s.min())
-        
-        t = datetime.datetime(simtime[0],simtime[1],simtime[2],
-                              simtime[3],simtime[4],simtime[5])
-        time.append(t)
+	time   = []
+	cpcp_n = []
+	cpcp_s = []
+	for file in sort(files):
+		print(file)
+		(simtime,
+		 x,y,
+		 psi_n,psi_s,
+		 fac_n,fac_s,
+		 sigmap_n,sigmap_s,
+		 sigmah_n,sigmah_s,
+		 energy_n,energy_s,
+		 flux_n,flux_s) = get_data(file)
+	
+		cpcp_n.append(psi_n.max()-psi_n.min())
+		cpcp_s.append(psi_s.max()-psi_s.min())
+		
+		t = datetime.datetime(simtime[0],simtime[1],simtime[2],
+							  simtime[3],simtime[4],simtime[5])
+		time.append(t)
 
 
-    return (time,cpcp_n,cpcp_s)
+	return (time,cpcp_n,cpcp_s)
 
 def get_time_series(directory):
-    from pylab import sort,sqrt,nonzero
-    import glob,datetime
-    import integrate
+	from pylab import sort,sqrt,nonzero
+	import glob,datetime
+	import integrate
 
-    files = glob.glob(directory+'*_mix_*.hdf')
-
-
-    ri=6500.e3
-    # Pre-compute some constants:
-    (simtime,x,y,psi_n,psi_s,fac_n,fac_s,sigmap_n,sigmap_s,sigmah_n,sigmah_s,
-     energy_n,energy_s,flux_n,flux_s) = get_data(files[0])
-    x[:,0]=0.0
-    y[:,0]=0.0
-    z=sqrt(1.0-x**2-y**2)
-    areaSphere = integrate.calcFaceAreas(x,y,z)*ri*ri
-
-    time   = []
-    cpcp_n = []
-    cpcp_s = []
-    fac_pos_n = []
-    fac_neg_n = []
-    fac_pos_s = []
-    fac_neg_s = []
-
-    for file in sort(files):
-        (simtime,
-         x,y,
-         psi_n,psi_s,
-         fac_n,fac_s,
-         sigmap_n,sigmap_s,
-         sigmah_n,sigmah_s,
-         energy_n,energy_s,
-         flux_n,flux_s) = get_data(file)
-    
-        cpcp_n.append(psi_n.max()-psi_n.min())
-        cpcp_s.append(psi_s.max()-psi_s.min())
-
-        pfac = fac_n.copy()
-        locs = nonzero(fac_n < 0)
-        pfac[locs] = 0.0
-        val = areaSphere*pfac[0:180,0:26]
-        fac_pos_n.append(val.sum()*1.e-12)
-
-        pfac = fac_n.copy()
-        locs = nonzero(fac_n > 0)
-        pfac[locs] = 0.0
-        val = areaSphere*pfac[0:180,0:26]
-        fac_neg_n.append(-val.sum()*1.e-12)
+	files = glob.glob(directory+'*_mix_*.hdf')
 
 
-        pfac = fac_s.copy()
-        locs = nonzero(fac_s < 0)
-        pfac[locs] = 0.0
-        val = areaSphere*pfac[0:180,0:26]
-        fac_pos_s.append(val.sum()*1.e-12)
+	ri=6500.e3
+	# Pre-compute some constants:
+	(simtime,x,y,psi_n,psi_s,fac_n,fac_s,sigmap_n,sigmap_s,sigmah_n,sigmah_s,
+	 energy_n,energy_s,flux_n,flux_s) = get_data(files[0])
+	x[:,0]=0.0
+	y[:,0]=0.0
+	z=sqrt(1.0-x**2-y**2)
+	areaSphere = integrate.calcFaceAreas(x,y,z)*ri*ri
 
-        pfac = fac_s.copy()
-        locs = nonzero(fac_s > 0)
-        pfac[locs] = 0.0
-        val = areaSphere*pfac[0:180,0:26]
-        fac_neg_s.append(-val.sum()*1.e-12)
+	time   = []
+	cpcp_n = []
+	cpcp_s = []
+	fac_pos_n = []
+	fac_neg_n = []
+	fac_pos_s = []
+	fac_neg_s = []
 
-        
-        t = datetime.datetime(simtime[0],simtime[1],simtime[2],
-                              simtime[3],simtime[4],simtime[5])
-        time.append(t)
+	for file in sort(files):
+		(simtime,
+		 x,y,
+		 psi_n,psi_s,
+		 fac_n,fac_s,
+		 sigmap_n,sigmap_s,
+		 sigmah_n,sigmah_s,
+		 energy_n,energy_s,
+		 flux_n,flux_s) = get_data(file)
+	
+		cpcp_n.append(psi_n.max()-psi_n.min())
+		cpcp_s.append(psi_s.max()-psi_s.min())
 
-    return (time,cpcp_n,cpcp_s,fac_pos_n,fac_neg_n,fac_pos_s,fac_neg_s)
+		pfac = fac_n.copy()
+		locs = nonzero(fac_n < 0)
+		pfac[locs] = 0.0
+		val = areaSphere*pfac[0:180,0:26]
+		fac_pos_n.append(val.sum()*1.e-12)
+
+		pfac = fac_n.copy()
+		locs = nonzero(fac_n > 0)
+		pfac[locs] = 0.0
+		val = areaSphere*pfac[0:180,0:26]
+		fac_neg_n.append(-val.sum()*1.e-12)
+
+
+		pfac = fac_s.copy()
+		locs = nonzero(fac_s < 0)
+		pfac[locs] = 0.0
+		val = areaSphere*pfac[0:180,0:26]
+		fac_pos_s.append(val.sum()*1.e-12)
+
+		pfac = fac_s.copy()
+		locs = nonzero(fac_s > 0)
+		pfac[locs] = 0.0
+		val = areaSphere*pfac[0:180,0:26]
+		fac_neg_s.append(-val.sum()*1.e-12)
+
+		
+		t = datetime.datetime(simtime[0],simtime[1],simtime[2],
+							  simtime[3],simtime[4],simtime[5])
+		time.append(t)
+
+	return (time,cpcp_n,cpcp_s,fac_pos_n,fac_neg_n,fac_pos_s,fac_neg_s)
 
 
 
 def efield(x,y,psi,ri): #Radius of ionosphere in m
-    import pylab as p
+	import pylab as p
 
-    phi=p.arctan2(y,x)
-    phi[phi<0]=phi[phi<0]+2*p.pi
+	phi=p.arctan2(y,x)
+	phi[phi<0]=phi[phi<0]+2*p.pi
    
-    # Fix phi at pole just in case
-    phi[:,0]=phi[:,1]
+	# Fix phi at pole just in case
+	phi[:,0]=phi[:,1]
 
-    theta=p.arcsin(p.sqrt(x**2+y**2))
+	theta=p.arcsin(p.sqrt(x**2+y**2))
 
-    dtheta=theta[0,1:]-theta[0,:-1]
-    dphi=phi[1:,1]-phi[:-1,1]
+	dtheta=theta[0,1:]-theta[0,:-1]
+	dphi=phi[1:,1]-phi[:-1,1]
 
-    ephi   = p.zeros(x.shape)
-    etheta = p.zeros(x.shape)
+	ephi   = p.zeros(x.shape)
+	etheta = p.zeros(x.shape)
 
-    phi_mid   = p.zeros(x.shape)
-    theta_mid = p.zeros(x.shape)
+	phi_mid   = p.zeros(x.shape)
+	theta_mid = p.zeros(x.shape)
 
-    nphi   = x.shape[0]
-    ntheta = x.shape[1]
+	nphi   = x.shape[0]
+	ntheta = x.shape[1]
 
-    for j in range(nphi-1): # loop over phi
-        theta_mid[j,:-1] = (theta[j,1:]+theta[j,:-1])/2.
-        etheta[j,:-1] =( (psi[j,1:]-psi[j,:-1])+(psi[j+1,1:]-psi[j+1,:-1]))/2./dtheta/ri*1.e6
+	for j in range(nphi-1): # loop over phi
+		theta_mid[j,:-1] = (theta[j,1:]+theta[j,:-1])/2.
+		etheta[j,:-1] =( (psi[j,1:]-psi[j,:-1])+(psi[j+1,1:]-psi[j+1,:-1]))/2./dtheta/ri*1.e6
 
-    for i in range(ntheta-1): # loop over theta
-        phi_mid[:-1,i] = ( phi[1:,i]+phi[:-1,i] )/2.
-        ephi[:-1,i] = 1./p.sin(theta_mid[0,i])*( (psi[1:,i]-psi[:-1,i])+(psi[1:,i+1]-psi[:-1,i+1]) )/2./dphi/ri*1.e6
+	for i in range(ntheta-1): # loop over theta
+		phi_mid[:-1,i] = ( phi[1:,i]+phi[:-1,i] )/2.
+		ephi[:-1,i] = 1./p.sin(theta_mid[0,i])*( (psi[1:,i]-psi[:-1,i])+(psi[1:,i+1]-psi[:-1,i+1]) )/2./dphi/ri*1.e6
 
-    # fixup the periodic axis
-    phi_mid[nphi-1,:]   = phi_mid[0,:]+2.*p.pi
-    theta_mid[nphi-1,:]   = theta_mid[0,:]
-    ephi[nphi-1,:] = ephi[0,:]
-    etheta[nphi-1,:] = etheta[0,:]
+	# fixup the periodic axis
+	phi_mid[nphi-1,:]   = phi_mid[0,:]+2.*p.pi
+	theta_mid[nphi-1,:]   = theta_mid[0,:]
+	ephi[nphi-1,:] = ephi[0,:]
+	etheta[nphi-1,:] = etheta[0,:]
 
-    # return the angular coordinates at cell centers (first tuple) and
-    # electric field in mV/m (second tuple)
-    return (phi_mid,theta_mid),(ephi,etheta)
+	# return the angular coordinates at cell centers (first tuple) and
+	# electric field in mV/m (second tuple)
+	return (phi_mid,theta_mid),(ephi,etheta)
 
 def joule(efield,sigmap):
-    from pylab import zeros
-    # Average conductance
-    sigp_mid = zeros(efield[0].shape)
-    sigp_mid[:-1,:-1] = 0.25*(sigmap[:-1,:-1]+sigmap[:-1,1:]+sigmap[1:,:-1]+sigmap[1:,1:])
+	from pylab import zeros
+	# Average conductance
+	sigp_mid = zeros(efield[0].shape)
+	sigp_mid[:-1,:-1] = 0.25*(sigmap[:-1,:-1]+sigmap[:-1,1:]+sigmap[1:,:-1]+sigmap[1:,1:])
 
-    # fixup the periodic axis
-    sigp_mid[-1,:]   = sigp_mid[0,:]
+	# fixup the periodic axis
+	sigp_mid[-1,:]   = sigp_mid[0,:]
 
-    return (sigp_mid*(efield[0]**2+efield[1]**2)*1.e-3)  # Joule heating in mW/m^2
+	return (sigp_mid*(efield[0]**2+efield[1]**2)*1.e-3)  # Joule heating in mW/m^2
 
 def jped(efield,sigmap):
-    from pylab import zeros,sqrt
-    # Average conductance
-    sigp_mid = zeros(efield[0].shape)
-    sigp_mid[:-1,:-1] = 0.25*(sigmap[:-1,:-1]+sigmap[:-1,1:]+sigmap[1:,:-1]+sigmap[1:,1:])
+	from pylab import zeros,sqrt
+	# Average conductance
+	sigp_mid = zeros(efield[0].shape)
+	sigp_mid[:-1,:-1] = 0.25*(sigmap[:-1,:-1]+sigmap[:-1,1:]+sigmap[1:,:-1]+sigmap[1:,1:])
 
-    # fixup the periodic axis
-    sigp_mid[-1,:]   = sigp_mid[0,:]
+	# fixup the periodic axis
+	sigp_mid[-1,:]   = sigp_mid[0,:]
 
-    return (sigp_mid*sqrt(efield[0]**2+efield[1]**2)*1.e-3)  # should be in microA/m
+	return (sigp_mid*sqrt(efield[0]**2+efield[1]**2)*1.e-3)  # should be in microA/m
 
 
--- scripts/remix_plot.py
+++ scripts/remix_plot.py
@@ -28,69 +28,71 @@ variables = { 'potential' : {'min':-100,
 import sys
 import argparse
 import numpy as np
-from astropy.time import Time
-import kaipy.kaiH5 as kaiH5
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from   astropy.time import Time
 
-TINY = 1.   # tolerance (in s) on time bounds to acount for the case where there's only one time slice
+import kaipy.kaiH5 as kaiH5
+import kaipy.remix.remix as remix
 
 parser = argparse.ArgumentParser()
 parser.add_argument('remixFile',help='REMIX file to use')
-parser.add_argument('-UT',"--UniversalTime",help="UT to plot in the format YYYY-MM-DDThh:mm:ss'")
+parser.add_argument('-n',type=int,help="Time slice to plot (similar to msphpic.py)")
+
 #Finalize parsing
 args = parser.parse_args()
  
 nsteps,sIds=kaiH5.cntSteps(args.remixFile)
 T=kaiH5.getTs(args.remixFile,sIds,aID='MJD')
 
-if not(args.UniversalTime):
+if not(args.n):
 	for i,tt in enumerate(T):
 		print('Step#%06d: '%sorted(sIds)[i],Time(tt,format='mjd').iso)
 	sys.exit(0)
 else:
-	t0 = Time(args.UniversalTime)
-	if (t0.mjd<T.min()-TINY) or (t0.mjd>T.max()+TINY):
-		sys.exit('Time outside bounds. Stopping. ')
-		
-	# find closest time
-	imin = np.argmin(np.abs(t0.mjd-T))
-	print('Found closest time:',Time(T[imin],format='mjd').iso)
+	if args.n not in sIds:
+			sys.exit("Time step not in the h5 file.")
+
+	print('Found time:',Time(T[args.n],format='mjd').iso)
 ################################################################
 
-# now plotting
+# Now plotting
 
-import kaipy.remix.remix as remix
-import sys
-from matplotlib.pyplot import rc,figure,figtext,subplot,show,pcolormesh,savefig
-from numpy import arctan2,sqrt,pi
+mpl.use('Agg')
+mpl.rc('mathtext',fontset='stixsans',default='regular')
+mpl.rc('font',size=10)
 
-rc('mathtext',fontset='stixsans',default='regular')
-rc('font',size=11)
-
-step = sorted(sIds)[imin]
-ion = remix.get_data(args.remixFile,step)
+ion = remix.get_data(args.remixFile,args.n)
 
 x = ion['X']
 y = ion['Y']
 
 for h in ['NORTH','SOUTH']:
-	figure(figsize=(10,6))
-	figtext(0.5,0.92,'MIX ('+h+')\n'+Time(T[imin],format='mjd').iso,
-			fontsize=14,multialignment='center')
+	fig = plt.figure(figsize=(12,7.5))
+	plt.figtext(0.5,0.94,'MIX ('+h+')\n'+Time(T[args.n],format='mjd').iso,
+			fontsize=14,multialignment='center',horizontalalignment='center')
 
-	theta=arctan2(y,x)
-	theta[theta<0]=theta[theta<0]+2*pi
-	r=sqrt(x**2+y**2)
+	# note, because of how the grid is set up in the h5 file,
+	# the code below produces the first theta that's just shy of 2pi.
+	# this is because the original grid is staggered at half-cells from data.
+	# empirically, this is OK for pcolormesh plots under remix.plot.
+	# however, contour plots have issues across the periodic boundary. 
+	# still working on it as of 24 Dec 2019
+	theta=np.arctan2(y,x)
+	theta[theta<0]=theta[theta<0]+2*np.pi
+	r=np.sqrt(x**2+y**2)
 
 	if (h.lower()=='north'):
 		variables['potential']['data'] = ion['Potential '+h]
-		variables['current']['data']   = ion['Field-aligned current '+h]
+		variables['current']['data']   =-ion['Field-aligned current '+h]  # note, converting to common convention (upward=positive)
 		variables['sigmap']['data']    = ion['Pedersen conductance '+h]
 		variables['sigmah']['data']    = ion['Hall conductance '+h]
 		variables['energy']['data']    = ion['Average energy '+h]
 		variables['flux']['data']      = ion['Number flux '+h]
 		# variables['efield']['data']    = efield_n*1.e6
 		# variables['joule']['data']     = sigmap_n*efield_n**2*1.e-3
-	else:
+	else:  # note flipping the y(phi)-axis
 		variables['potential']['data'] = ion['Potential '+h][:,::-1]
 		variables['current']['data']   = ion['Field-aligned current '+h][:,::-1]
 		variables['sigmap']['data']    = ion['Pedersen conductance '+h][:,::-1]
@@ -98,30 +100,29 @@ for h in ['NORTH','SOUTH']:
 		variables['energy']['data']    = ion['Average energy '+h][:,::-1]
 		variables['flux']['data']      = ion['Number flux '+h][:,::-1]
 	
-	subplot(231,polar=True)
-	remix.plot(theta,r,variables,'potential')
+	gs = gridspec.GridSpec(2,3,figure=fig,left=0.03,right=0.97, top=0.9,bottom=0.03)
+
+	ax1=fig.add_subplot(gs[0,0],polar=True)
+	remix.plot(theta,r,variables,'potential',ax=ax1)
 	
-	subplot(234,polar=True)
-	remix.plot(theta,r,variables,'current')
+	ax2=fig.add_subplot(gs[1,0],polar=True)
+	remix.plot(theta,r,variables,'current',ax=ax2,x=x,y=y)
 
-	subplot(232,polar=True)
-	remix.plot(theta,r,variables,'sigmap')
+	ax3=fig.add_subplot(gs[0,1],polar=True)
+	remix.plot(theta,r,variables,'sigmap',ax=ax3)
 
-	subplot(235,polar=True)
-	remix.plot(theta,r,variables,'sigmah')
+	ax4=fig.add_subplot(gs[1,1],polar=True)
+	remix.plot(theta,r,variables,'sigmah',ax=ax4)
 
-	subplot(233,polar=True)
-	remix.plot(theta,r,variables,'energy')
+	ax5=fig.add_subplot(gs[0,2],polar=True)
+	remix.plot(theta,r,variables,'energy',ax=ax5)
 
-	subplot(236,polar=True)
-	remix.plot(theta,r,variables,'flux')
+	ax6=fig.add_subplot(gs[1,2],polar=True)
+	remix.plot(theta,r,variables,'flux',ax=ax6)
 
 	if (h.lower()=='north'):
-		savefig('remix_n.png')
+		plt.savefig('remix_n.png',dpi=300)
 	else:
-		savefig('remix_s.png')
-
-
-show()
+		plt.savefig('remix_s.png',dpi=300)
 
 
