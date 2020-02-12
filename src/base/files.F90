d641161b5b535dc016f58d2575e422f96b6e611a
vmerkin <slava.merkin@jhuapl.edu>
2020-02-12 09:56:28 -0500

    Fix for gfortran.

--- src/base/files.F90
+++ src/base/files.F90
@@ -16,12 +16,12 @@ module files
         character(len=*), intent(in) :: fIn
         logical :: CheckDir
 
-#IFDEF __INTEL_COMPILER
+#ifdef __INTEL_COMPILER
         inquire(directory=fIn,exist=CheckDir) !Intel only
-#ELSE
+#else
         !Might work for gfortran
         inquire(file=trim(fIn)//'/.',exist=CheckDir)
-#ENDIF
+#endif
     end function CheckDir
 
     subroutine CheckDirOrMake(fIn)
