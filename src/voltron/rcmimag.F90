a0539ccc2b223a2c00f569b603e2e81736f13bdf
Kareem Sorathia <soratka1@jhuapl.edu>
2020-02-13 11:49:10 -0500

    Fixing RCM domain bug

--- src/voltron/rcmimag.F90
+++ src/voltron/rcmimag.F90
@@ -246,7 +246,8 @@ module rcmimag
         do j=1,RCMApp%nLon_ion
             RCMApp%toMHD(jBnd(j):,j) = .true.
         enddo
-        RCMApp%toMHD = RCMApp%toMHD .and. (RCMApp%iopen(i,j) == RCMTOPCLOSED)
+
+        RCMApp%toMHD = RCMApp%toMHD .and. (RCMApp%iopen == RCMTOPCLOSED)
         
     end subroutine SetIngestion
 
