d308350c44f0f547c28e5d2523fa7d7a8badb849
Kareem Sorathia <soratka1@jhuapl.edu>
2020-04-08 14:42:15 -0400

    Adding option for old-style naming

--- kaipy/kaiH5.py
+++ kaipy/kaiH5.py
@@ -4,14 +4,21 @@ import numpy as np
 
 #Generate MPI-style name
 def genName(bStr,i,j,k,Ri,Rj,Rk,nRes=None):
-	#n = j + i*Rj + k*Ri*Rj
 	n = k + j*Rk + i*Rj*Rk
 	if (nRes is None):
 		fID = bStr + "_%04d_%04d_%04d_%04d_%04d_%04d.gam.h5"%(Ri,Rj,Rk,i,j,k)
 	else:
 		fID = bStr + "_%04d_%04d_%04d_%04d_%04d_%04d"%(Ri,Rj,Rk,i,j,k)+".Res.%05d.h5"%(nRes)
 	return fID
-	
+#Generate old-style MPI name
+def genNameOld(bStr,i,j,k,Ri,Rj,Rk,nRes=None):
+	n = k + j*Rk + i*Rj*Rk
+	if (nRes is None):
+		fID = bStr + "_%04d_%04d_%04d_%04d_%04d_%04d_%012d.h5"%(Ri,Rj,Rk,i,j,k,n)
+	else:
+		fID = bStr + "_%04d_%04d_%04d_%04d_%04d_%04d_%012d"%(Ri,Rj,Rk,i,j,k,n)+".Res.%05d.h5"%(nRes)
+	return fID
+
 #Quick check and exit routine
 def CheckOrDie(fname):
 	import os.path
