4bdb57ce935f8724842055416769b48a035421d3
Kareem Sorathia <soratka1@jhuapl.edu>
2020-04-23 09:10:26 -0400

    First steps

--- src/rcm/precision.f90
+++ src/rcm/precision.f90
@@ -9,4 +9,10 @@ module rcm_precision
     INTEGER(iprec), parameter :: ICONWRITERESTART = 31337
     INTEGER(iprec), parameter :: ICONWRITEOUTPUT = ICONWRITERESTART + 1
     INTEGER(iprec), parameter :: ICONRESTART     = ICONWRITERESTART - 1
+    INTEGER(iprec), parameter :: RCMELECTRON = 1
+    INTEGER(iprec), parameter :: RCMPROTON   = 2
+    INTEGER(iprec), parameter :: RCMCLAWLIM = 1
+	!Limiters: 1=minmod, 2=superbee, 3=Van Leer, 4=MC, 5=Beam Warming
+	!           6=Fromm, 7=Albada2, 8=Albada3, 9=Ultrabee, 10=UltrabeeMod, 11=Arora Roe
+
 end module rcm_precision
