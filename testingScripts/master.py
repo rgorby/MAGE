a6dd21c94ba55ffa16ab72296d4ebac82077834e
Jake Hoffman <jhoffman@cheyenne2.cheyenne.ucar.edu>
2020-12-04 13:22:38 -0700

    Added master and unit test scripts

---
+++ testingScripts/master.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import subprocess
+from os.path import expanduser
+sys.path.insert(1, "./python-slackclient")
+from slack import WebClient
+from slack.errors import SlackApiError
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+# Get Slack API token
+slack_token = os.environ["SLACK_BOT_TOKEN"]
+print(slack_token)
+client = WebClient(token=slack_token)
+
+# Get the home directory
+home = expanduser("~")
+
+# Change directory to Kaiju repo
+os.chdir(home)
+os.chdir("kaiju")
+# Delete all build folders
+os.system("rm -rf build*/")
+os.system('ls')
+
+# Git Status and then attempt to pull
+os.system('git status')
+print('Attempting git pull via subprocess...')
+p = subprocess.Popen("git pull", shell=True, stdout=subprocess.PIPE)
+text = p.stdout.read()
+text = text.decode('ascii')
+text = text.rstrip()
+print(text)
+isTest = False
+
+# Check argument flags
+if (len(sys.argv) < 2):
+    # If no arguments, check for update
+    if (text == 'Already up to date.'):
+        # Try to send Slack message
+        try:
+            response = client.chat_postMessage(
+                channel="#kaijudev",
+                text='No test today. It is already up to date!',
+            )
+        except SlackApiError as e:
+            # You will get a SlackApiError if "ok" is False
+            assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+        
+        exit()
+# Else check for force flag
+elif(str(sys.argv[1]) == '-f'):
+    print("Buuuuut you forced me to do it anyway...")
+# Else check for testing flag
+elif(str(sys.argv[1]) == '-t'):
+    print("Test Mode: On")
+    isTest = True
+
+os.chdir("testingScripts")
+
+if (isTest == True):
+    buildTest = subprocess.Popen("python3 buildTest.py -t", shell = True)
+
+elif (not len(sys.argv) < 2):
+    buildTest = subprocess.Popen("python3 buildTest.py -f", shell = True)
+
+else:
+    buildTest = subprocess.Popen("python3 buildTest.py", shell = True)
