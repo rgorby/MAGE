eb3b1e565ecc7a1c2218cbd5996a4c11b3a434b6
Elena P <eprovorn@gmail.com>
2021-02-03 15:00:21 -0500

    helio viz scripts

---
+++ kaipy/gamhelio/heliosphere.py
@@ -0,0 +1,248 @@
+#Various tools to post-process and analyze Gamera heliosphere runs
+from kaipy.kdefs import *
+import kaipy.gamera.gampp
+from kaipy.gamera.gampp import GameraPipe
+import numpy as np
+import glob
+import kaipy.kaiH5 as kh5
+import timeit
+
+#Object to pull from MPI/Serial heliosphere runs (H5 data), extends base
+
+ffam =  "monospace"
+dLabC = "black" #Default label color
+dLabFS = "medium" #Default label size
+dBoxC = "lightgrey" #Default box color
+TINY = 1.0e-8
+rmStr = "mixtest"
+
+#Assuming LFM/EGG type grid -need to adapt to helio grid
+class GamsphPipe(GameraPipe):
+	#Initialize object, rely on base class, take optional unit identifier
+	def __init__(self,fdir,ftag,doFast=False,uID="Inner"):
+
+		print("Initializing %s heliosphere"%(uID))
+		
+		#units for helio
+		#self.MagM = -EarthM0g*1.0e+5
+		self.bScl = 100.    #->nT
+		#self.pScl = 1.67e-2 #->nPa
+		self.vScl = 150.  #-> km/s
+		self.tScl = 4637.    #->seconds
+		self.dScl = 200. #cm-3
+
+		#2D equatorial grid, stretched polar (Ni,Nj*2+1)
+		#??????
+		#2D equatorial grid
+		self.xxi = [] ; self.yyi = [] #corners
+		self.xxc = [] ; self.yyc = [] #centers
+
+		#base class, will use OpenPipe below
+		GameraPipe.__init__(self,fdir,ftag,doFast=doFast)
+
+		self.Rin = self.xxi[0,0]
+		
+	def OpenPipe(self,doVerbose=True):
+		GameraPipe.OpenPipe(self,doVerbose)
+		
+		if (self.UnitsID != "CODE"):
+			self.bScl   = 1.0  #->nT
+			#self.pScl   = 1.0  #->nPa
+			self.vScl   = 1.0  #-> km/s
+			self.tScl   = 1.0 #->Seconds
+			# [EP] added
+			self.dScl = 1.0 
+			#self.tRMScl = 63.8 #->Seconds
+
+		#Rescale time
+		self.T = self.tScl*self.T
+
+		Neq_a = self.Nj/2 #cell above eq plane
+
+		Nr = self.Ni
+		Np = self.Nk
+
+		#corners in eq XY plane
+		self.xxi = np.zeros((Nr+1,Np+1)) 
+		self.yyi = np.zeros((Nr+1,Np+1))
+		#centers
+		self.xxc = np.zeros((Nr  ,Np  ))
+		self.yyc = np.zeros((Nr  ,Np  ))
+		
+
+		#equatorial plane
+		#corners i,k in eq plane, j index is Neq_a
+		self.xxi[:,:] = self.X[:,Neq_a,:]
+		self.yyi[:,:] = self.Y[:,Neq_a,:]
+
+		#centers i,k
+		self.xxc = 0.25*(self.xxi[:-1,:-1] + self.xxi[1:,:-1] + self.xxi[:-1,1:] + self.xxi[1:,1:])
+		self.yyc = 0.25*(self.yyi[:-1,:-1] + self.yyi[1:,:-1] + self.yyi[:-1,1:] + self.yyi[1:,1:])
+		r = np.sqrt(self.xxc**2.0 + self.yyc**2.0)
+
+		#!!!for helio meridional plane grid would be different; should be done like for magnetosphere
+
+
+		#Upper half plane
+		#for j in range(self.Nj):
+		#	self.xxi[:,j] = self.X[:,j,0] #k=0
+		#	self.yyi[:,j] = self.Y[:,j,0]
+		#Lower half plane
+		#for j in range(self.Nj,Np+1):
+		#	jp = Np-j
+		#	self.xxi[:,j] =  self.X[:,jp,0]
+		#	self.yyi[:,j] = -self.Y[:,jp,0]
+
+		if (self.hasMJD):
+			print("Found MJD data")
+			print("\tTime (Min/Max) = %f/%f"%(self.MJDs.min(),self.MJDs.max()))
+
+	def EqSlice(self,vID,sID=None,vScl=None,doEq=True,doVerb=True):
+		#Get full 3D variable first
+		Q = self.GetVar(vID,sID,vScl,doVerb)
+
+		Nj2 = self.Nj//2 
+
+		#above and below the eq plane
+		ja = Nj2 - 1
+		jb = ja + 1
+
+		Nr = self.Ni
+		Np = self.Nk
+
+		#equatorial j-slice of var 
+		Qj = np.zeros((Nr,Np))
+		#taking average above/below eq plane
+		Qj[:,:] = 0.5*( Q[:,ja,:] + Q[:,jb,:] )
+		return Qj
+
+	#Equatorial speed (in km/s) in eq plane
+	def eqMagV(self,s0=0):
+		Vx = self.EqSlice("Vx",s0) #Unscaled
+		Vy = self.EqSlice("Vy",s0) #Unscaled
+		Vz = self.EqSlice("Vz",s0) #Unscaled
+		Veq = self.vScl*np.sqrt(Vx**2.0+Vy**2.0+Vz**2.0)
+		return Veq
+
+	#Normalized density (D*r*r/21.5/21.5 in cm-3) in eq plane
+	def eqNormD (self,s0=0):
+		#self.GetGrid(self,doVerb=True)
+
+		D = self.EqSlice("D",s0) #Unscaled
+		#radius of inner boudnary (center of the first cell)
+		R0 = self.xxc[0,0]
+		print ("R0 = %f"%R0)
+
+		#calculate normalized density
+		#r = np.sqrt(self.xxc**2.0 + self.yyc**2.0)
+		Norm = r*r/R0/R0
+		NormDeq = self.dScl*D*Norm
+		return NormDeq
+
+	#Normalized Br (Br*r*r/21.5/21.5) in eq plane
+	def eqNormBr (self,s0=0):
+		Br = self.EqSlice("Br",s0) #Unscaled
+		R0 = self.xxc[0,0]
+		print ("R0 = %f"%R0)
+
+		#calculate normalized Br
+		#r = np.sqrt(self.xxc**2.0 + self.yyc**2.0)
+		Norm = r*r/R0/R0
+		NormBreq = self.bScl*Br*Norm
+		return NormBreq
+
+	#Temperature
+
+
+
+	#Return data for meridional 2D field lines
+	#Need to use Cartesian grid
+	def bStream(self,s0=0,xyBds=[-35,25,-25,25],dx=0.05):
+		
+		#Get field data
+		U = self.bScl*self.EggSlice("Bx",s0,doEq=False)
+		V = self.bScl*self.EggSlice("Bz",s0,doEq=False)
+	
+		x1,y1,gu,gv,gM = self.doStream(U,V,xyBds,dx)
+		return x1,y1,gu,gv,gM
+
+	def vStream(self,s0=0,xyBds=[-35,25,-25,25],dx=0.05):
+		#Get field data
+		U = self.vScl*self.EggSlice("Vx",s0,doEq=True)
+		V = self.vScl*self.EggSlice("Vy",s0,doEq=True)
+
+		x1,y1,gu,gv,gM = self.doStream(U,V,xyBds,dx)
+		return x1,y1,gu,gv,gM
+
+
+	#Add time label, xy is position in axis (not data) coords
+	def AddTime(self,n,Ax,xy=[0.9,0.95],cLab=dLabC,fs=dLabFS,T0=0.0,doBox=True,BoxC=dBoxC):
+		ffam = "monospace"
+		HUGE = 1.0e+8
+		#Decide whether to do UT or elapsed
+		if (self.hasMJD):
+			minMJD = self.MJDs[n-self.s0]
+		else:
+			minMJD = -HUGE
+		if (self.hasMJD and minMJD>TINY):
+			from astropy.time import Time
+			dtObj = Time(self.MJDs[n-self.s0],format='mjd').datetime
+			tStr = "  " + dtObj.strftime("%H:%M:%S") + "\n" + dtObj.strftime("%m/%d/%Y")
+
+		else:	
+			#Get time in seconds
+			t = self.T[n-self.s0] - T0
+			Nm = np.int( (t-T0)/60.0 ) #Minutes, integer
+			Hr = Nm/60
+			Min = np.mod(Nm,60)
+			Sec = np.mod(np.int(t),60)
+
+			tStr = "Elapsed Time\n  %02d:%02d:%02d"%(Hr,Min,Sec)
+		if (doBox):
+			Ax.text(xy[0],xy[1],tStr,color=cLab,fontsize=fs,transform=Ax.transAxes,family=ffam,bbox=dict(boxstyle="round",fc=dBoxC))
+
+	#def AddSW(self,n,Ax,xy=[0.725,0.025],cLab=dLabC,fs=dLabFS,T0=0.0,doBox=True,BoxC=dBoxC,doAll=True):
+	#	import kaipy.kaiH5 as kh5
+	#	#Start by getting SW data
+	#	vIDs = ["D","P","Vx","Bx","By","Bz"]
+	#	Nv = len(vIDs)
+	#	qSW = np.zeros(Nv)
+	#	if (self.isMPI):
+	#		fSW = self.fdir + "/" + kh5.genName(self.ftag,self.Ri-1,0,0,self.Ri,self.Rj,self.Rk)
+	#	else:
+	#		fSW = self.fdir + "/" + self.ftag + ".h5"
+	#
+	#	for i in range(Nv):
+	#		Q = kh5.PullVar(fSW,vIDs[i],n)
+	#		qSW[i] = Q[-1,0,0]
+	#	D = qSW[0] ; P = qSW[1] ; Vx = qSW[2] ; Bx = qSW[3] ; By = qSW[4] ; Bz = qSW[5]
+	#	SWStr = "Solar Wind\n"
+	#	MagB = self.bScl*np.sqrt(Bx**2.0+By**2.0+Bz**2.0)
+	#	#Clock = atan(by/bz), cone = acos(Bx/B)
+	#	r2deg = 180.0/np.pi
+	#	if (MagB>TINY):
+	#		clk  = r2deg*np.arctan2(By,Bz)
+	#		cone = r2deg*np.arccos(self.bScl*Bx/MagB)
+	#	else:
+	#		clk = 0.0
+	#		cone = 0.0
+	#	if (clk<0):
+	#		clk = clk+360.0
+	#	Deg = r"$\degree$"
+	#	SWStr = "Solar Wind\nIMF: %4.1f [nT], %5.1f"%(MagB,clk) + Deg# + ", %5.2f"%(cone) + Deg
+	#	if (doAll):
+	#		SWStr = SWStr + "\nDensity: %5.1f [#/cc] \nSpeed:  %6.1f [km/s] "%(D,self.vScl*np.abs(Vx))
+
+	#	if (doBox):
+	#		Ax.text(xy[0],xy[1],SWStr,color=cLab,fontsize=fs,transform=Ax.transAxes,family=ffam,bbox=dict(boxstyle="round",fc=dBoxC))
+	#	else:
+	#		Ax.text(xy[0],xy[1],SWStr,color=cLab,fontsize=fs,transform=Ax.transAxes,family=ffam,bbox=dict(boxstyle="round",fc=dBoxC))
+	#def AddCPCP(self,n,Ax,xy=[0.9,0.95],cLab=dLabC,fs=dLabFS,doBox=True,BoxC=dBoxC):
+	#	cpcp = self.GetCPCP(n)
+	#	tStr = "CPCP   (North/South)\n%6.2f / %6.2f [kV]"%(cpcp[0],cpcp[1])
+	#	if (doBox):
+	#		Ax.text(xy[0],xy[1],tStr,color=cLab,fontsize=fs,transform=Ax.transAxes,family=ffam,bbox=dict(boxstyle="round",fc=dBoxC))
+	#	else:
+	#		Ax.text(xy[0],xy[1],tStr,color=cLab,fontsize=fs,transform=Ax.transAxes,family=ffam)
+
+	
---
+++ scripts/heliopic.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+#Make a quick figure of a Gamera helio run
+
+import argparse
+from argparse import RawTextHelpFormatter
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import kaipy.kaiViz as kv
+#from mpl_toolkits.axes_grid1.inset_locator import inset_axes #do not need for helio if not inserting anything
+import matplotlib.gridspec as gridspec
+import numpy as np
+
+import kaipy.gamhelio.helioViz as hviz
+import kaipy.gamhelio.heliosphere as hsph
+import kaipy.gamera.gampp as gampp #serial/MPI gamera output
+import os
+
+if __name__ == "__main__":
+	#Defaults
+	fdir = os.getcwd()
+	ftag = "helio"
+	nStp = -1
+	fOut = "qkpichelio.png"
+	doDen = False
+	#noIon = False
+	noMPI = False
+	#doJy = False
+	#doBz = False
+	MainS = """Creates simple multi-panel figure for Gamera helio run
+	Top Panel - density and radial velocity in equatorial plane
+	Bottom Panel - density and radial velocity in meridional plane
+	"""
+
+	# For helio we can choose what to show by default and have a couple of options "instead" defaults
+
+	parser = argparse.ArgumentParser(description=MainS, formatter_class=RawTextHelpFormatter)
+	parser.add_argument('-d',type=str,metavar="directory",default=fdir,help="Directory to read from (default: %(default)s)")
+	parser.add_argument('-id',type=str,metavar="runid",default=ftag,help="RunID of data (default: %(default)s)")
+	parser.add_argument('-n' ,type=int,metavar="step" ,default=nStp,help="Time slice to plot (default: %(default)s)")
+	#parser.add_argument('-bz'   , action='store_true', default=doBz ,help="Show Bz instead of dBz (default: %(default)s)")
+	parser.add_argument('-den'  , action='store_true', default=doDen,help="Show density instead of pressure (default: %(default)s)")
+	#parser.add_argument('-jy'   , action='store_true', default=doJy ,help="Show Jy instead of pressure (default: %(default)s)")
+	#parser.add_argument('-noion', action='store_true', default=noIon,help="Don't show ReMIX data (default: %(default)s)")
+	parser.add_argument('-nompi', action='store_true', default=noMPI,help="Don't show MPI boundaries (default: %(default)s)")
+	
+	#size of domain - do not need right now. Think how it can be useful.
+	#hviz.AddSizeArgs(parser)
+
+	#Finalize parsing
+	args = parser.parse_args()
+	fdir = args.d
+	ftag = args.id
+	nStp = args.n
+	doDen = args.den
+	#noIon = args.noion
+	noMPI = args.nompi
+	doMPI = (not noMPI)
+	#doJy = args.jy
+	#doBz = args.bz
+
+	#Get domain size
+	xyBds = hviz.GetSizeBds()
+	print (xyBds)
+
+	#Specify domain size!!!!!!!
+
+
+	#---------------------
+	#Do work
+	doFast=False
+	#doIon = not noIon
+
+	#---------
+	#Figure parameters
+	figSz = (12,7.5)
+	
+	#======
+	#Init data
+	gsph = hsph.GamsphPipe(fdir,ftag,doFast=doFast)
+
+	if (nStp<0):
+		#assume this is working for helo but who knows
+		nStp = hsph.sFin
+		print("Using Step %d"%(nStp))
+
+	print (nStp)
+
+	#======
+	#Setup figure
+	fig = plt.figure(figsize=figSz)
+	gs = gridspec.GridSpec(2,6,height_ratios=[20,1,1],hspace=0.025)
+	
+
+	AxL = fig.add_subplot(gs[0,0:3])
+	AxR = fig.add_subplot(gs[0,3:])
+
+	#colorbars
+	AxC1 = fig.add_subplot(gs[-1,0:3])
+	AxC2 = fig.add_subplot(gs[-1,3:])
+	#AxC3 = fig.add_subplot(gs[-1,4:6])
+
+	#"FAC" color bar
+	#cbM = kv.genCB(AxC2,kv.genNorm(remix.facMax),"FAC",cM=remix.facCM,Ntk=4)
+	#AxC2.xaxis.set_ticks_position('top')
+
+
+	#plotting equatorial magV (left plot in msphere pic)
+	Vz = hviz.PlotEqMagV(gsph,nStp,xyBds,AxL,AxC1)
+
+	hviz.PlotEqMagV(gsph,nStp,xyBds,AxR,AxC2)
+
+	#if (doJy):
+	#	mviz.PlotJyXZ(gsph,nStp,xyBds,AxR,AxC3)
+	#else:
+	#plotting meridional slice (right plot in msphere pic)
+	#mviz.PlotMerid(gsph,nStp,xyBds,AxR,doDen,doRCM,AxC3)
+	
+	#Add time (upper left) - Later!!!
+	hsph.AddTime(nStp,AxL,xy=[0.025,0.89],fs="x-large")
+
+
+	#Add wsa info (lower left) instead of Solar wind params
+	#gsph.AddSW(nStp,AxL,xy=[0.625,0.025],fs="small")
+
+	#Add inset RCM plot
+	#if (doRCM):
+	#	AxRCM = inset_axes(AxL,width="30%",height="30%",loc=3)
+	#	rcmpp.RCMInset(AxRCM,rcmdata,nStp,mviz.vP)
+	#	#Add some dBz contours
+	#	AxRCM.contour(kv.reWrap(gsph.xxc),kv.reWrap(gsph.yyc),kv.reWrap(Bz),[0.0],colors=mviz.bz0Col,linewidths=mviz.cLW)
+	#	#AxRCM.contour(kv.reWrap(gsph.xxc),kv.reWrap(gsph.yyc),kv.reWrap(dBz),dbzVals,norm=vDB,cmap=mviz.dbCM,linewidths=0.25)
+	#	rcmpp.AddRCMBox(AxL)
+
+	#if (doIon):
+	#	gsph.AddCPCP(nStp,AxR,xy=[0.610,0.925])
+
+	#if (doIon):
+	#	mviz.AddIonBoxes(gs[0,3:],ion)
+
+	#Add MPI decomp
+	if (doMPI):
+		hviz.PlotMPI(gsph,AxL)
+		hviz.PlotMPI(gsph,AxR)
+
+	kv.savePic(fOut,bLenX=45)
