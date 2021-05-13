5aa996cfe08c4e50980719fb7c813c9966525542
wiltbemj <wiltbemj@ucar.edu>
2021-05-13 12:41:41 -0400

    Moves subroutines for Sat comp into kaiTools and kaiViz

--- kaipy/kaiTools.py
+++ kaipy/kaiTools.py
@@ -2,8 +2,19 @@ import numpy as np
 import datetime
 import os
 import glob
-from astropy.time import Time
+import subprocess
 from xml.dom import minidom
+
+#import spacepy and cdasws
+import spacepy
+from spacepy.coordinates import Coords
+from spacepy.time import Ticktock
+import spacepy.datamodel as dm
+from cdasws import CdasWs
+from astropy.time import Time
+
+#import hdf5
+import h5py
 
 def MJD2UT(mjd):
 	astroT = Time(mjd,format='mjd').iso
@@ -204,20 +215,19 @@ def getScIds():
 						'magData': 'B_vec_xyz_gse__C4_CP_FGM_SPIN',
 						'magCoordSys': 'GSE'
 						}
-	scDic['THEMISA'] = {'ephemId':'THA_L1_STATE',
-						'ephemData':'tha_pos_gsmV',
-						'ephemCoordSys':'GSM',
-						'denId':'THA_L2_MOM',
-						'denData':'tha_peim_density',
-						'presId': 'THA_L2_MOM',
-						'presData': 'tha_peim_ptot',
-						'velId': 'THA_L2_MOM',
-						'velData':'tha_peim_velocity_gsm',
-						'velCoordSys':'GSM',
-						'magId': 'THA_L2_MOM',
-						'magData': 'tha_peim_mag',
-						'magCoordSys': 'GSM'
-						}
+#	scDic['THEMISA'] = {'ephemId':'THA_L1_STATE',
+#						'ephemData':'tha_pos_gsmV',
+#						'ephemCoordSys':'GSM',
+#						'denId':'THA_L2_MOM',
+#						'denData':'tha_peim_density',
+#						'presId': 'THA_L2_MOM',
+#						'presData': 'tha_peim_ptot',
+#						'velId': 'THA_L2_MOM',
+#						'velData':'tha_peim_velocity_gsm',
+#						'velCoordSys':'GSM',
+#						'magId': 'THA_L2_MOM',
+#						'magCoordSys': 'GSM'
+#						}
 	scDic['MMS1'] = {'ephemId':'MMS1_FGM_SRVY_L2',
 						'ephemData':'mms1_fgm_r_gsm_srvy_l2',
 						'ephemCoordSys':'GSM',
@@ -276,6 +286,170 @@ def getScIds():
 						}
 
 	return scDic
+
+def pullVar(cdaObsId,cdaDataId,t0,t1,deltaT):
+	cdas = CdasWs()
+	status,data =  cdas.get_data(cdaObsId,cdaDataId,t0,t1,
+								 binData={
+									 'interval': deltaT,
+									 'interpolateMissingValues': True,
+									 'sigmaMultipler': 4})
+	return status,data
+
+def addVar(mydata,scDic,varname,idname,dataname,t0,t1,deltaT):
+	#print(scDic,varname,idname,dataname,scDic[idname])
+	if scDic[idname] is not None:
+		status,data = pullVar(scDic[idname],scDic[dataname],t0,t1,deltaT)
+		#print(status)
+		if status['http']['status_code'] == 200:
+			mydata[varname] = dm.dmarray(data[scDic[dataname]],
+													 attrs=data[scDic[dataname]].attrs)
+			#mydata.tree(attrs=True)
+	else:
+		#Mimic the cdasws return code for case when id isn't provided
+		status = {'http': {'status_code': 404}}
+	return status
+
+def getSatData(scDic,t0,t1,deltaT):
+	#First get the empheris data if it doesn't exist return the failed status code and
+	#go no further
+	status,data = pullVar(scDic['ephemId'],scDic['ephemData'],t0,t1,deltaT)
+	if status['http']['status_code'] != 200:
+		print('Unable to get data for ', scDic['ephemId'])
+		return status,data
+	else:
+		#data.tree(attrs=True)
+		mydata = dm.SpaceData(attrs={'Satellite':data.attrs['Source_name']})
+		if 'Epoch_bin' in data.keys():
+			#print('Using Epoch_bin')
+			mytime = data['Epoch_bin']
+		elif 'Epoch' in data.keys():
+			#print('Using Epoch')
+			mytime = data['Epoch']
+		elif ([key for key in data.keys() if key.endswith('_state_epoch')]):
+			mytime = data[[key for key in data.keys()
+			if key.endswith('_state_epoch')][0]]
+		else:
+			print('Unable to determine time type')
+			status = {'http': {'status_code': 404}}
+			return status,data
+		mydata['Epoch_bin'] = dm.dmarray(mytime,
+										 attrs=mytime.attrs)
+		mydata['Ephemeris'] = dm.dmarray(data[scDic['ephemData']],
+										 attrs= data[scDic['ephemData']].attrs)
+		status1 = addVar(mydata,scDic,'MagneticField','magId','magData',
+			t0,t1,deltaT)
+		status1 = addVar(mydata,scDic,'Velocity','velId','velData',
+			t0,t1,deltaT)
+		status1 = addVar(mydata,scDic,'Density','denId','denData',
+			t0,t1,deltaT)
+		status1 = addVar(mydata,scDic,'Pressure','presId','presData',
+			t0,t1,deltaT)
+		#Add any metavar since they might be needed for unit/label determination
+		search_key = 'metavar'
+		res = [key for key,val in data.items() if search_key in key]
+		for name in res:
+			mydata[name] = dm.dmarray(data[name],attrs=data[name].attrs)
+
+	return status,mydata
+
+def convertGameraVec(x,y,z,ut,fromSys,fromType,toSys,toType):
+	invec = Coords(np.column_stack((x,y,z)),fromSys,fromType)
+	invec.ticks = Ticktock(ut)
+	outvec = invec.convert(toSys,toType)
+	return outvec
+
+def extractGAMERA(data,scDic,mjd0,sec0,fdir,ftag,cmd,keep):
+	Re = 6380.0
+	toRe = 1.0
+	if 'UNITS' in data['Ephemeris'].attrs:
+		if "km" in data['Ephemeris'].attrs['UNITS']:
+			toRe = 1.0/Re
+	elif 'UNIT_PTR' in data['Ephemeris'].attrs:
+		if data[data['Ephemeris'].attrs['UNIT_PTR']][0]:
+			toRe = 1.0/Re
+	if 'SM' == scDic['ephemCoordSys']:
+		smpos = Coords(data['Ephemeris'][:,0:3]*toRe,'SM','car')
+		smpos.ticks = Ticktock(data['Epoch_bin'])
+	elif 'GSM' == scDic['ephemCoordSys'] :
+		scpos = Coords(data['Ephemeris'][:,0:3]*toRe,'GSM','car')
+		scpos.ticks = Ticktock(data['Epoch_bin'])
+		smpos = scpos.convert('SM','car')
+	elif 'GSE'== scDic['ephemCoordSys']:
+		scpos = Coords(data['Ephemeris'][:,0:3]*toRe,'GSE','car')
+		scpos.ticks = Ticktock(data['Epoch_bin'])
+		smpos = scpos.convert('SM','car')
+	else:
+		print('Coordinate system transformation failed')
+		return
+	elapsedSecs = (smpos.ticks.getMJD()-mjd0)*86400.0+sec0
+	scId = scDic['ephemId']
+	fOut = os.path.join(fdir,scId+"sctrack.h5")
+	with h5py.File(fOut,'w') as hf:
+		hf.create_dataset("T" ,data=elapsedSecs)
+		hf.create_dataset("X" ,data=smpos.x)
+		hf.create_dataset("Y" ,data=smpos.y)
+		hf.create_dataset("Z" ,data=smpos.z)
+	chimpxml = genSCXML(fdir,ftag,
+		scid=scId,h5traj=os.path.basename(fOut))
+	xmlfile = os.path.join(fdir,scId+'.xml')
+	with open(xmlfile,"w") as f:
+		f.write(chimpxml.toprettyxml())
+
+	sctrack = subprocess.run([cmd, xmlfile], cwd=fdir,
+							stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+							text=True)
+
+	h5name = os.path.join(fdir, scId + '.sc.h5')
+	h5file = h5py.File(h5name, 'r')
+	ut = MJD2UT(h5file['MJDs'][:])
+
+	bx = h5file['Bx']
+	by = h5file['By']
+	bz = h5file['Bz']
+	if scDic['magCoordSys'] is None:
+		toCoordSys = 'GSM'
+	else:
+		toCoordSys = scDic['magCoordSys']
+	lfmb_out = convertGameraVec(bx[:],by[:],bz[:],ut,
+		'SM','car',toCoordSys,'car')
+	data['GAMERA_MagneticField'] = dm.dmarray(lfmb_out.data,
+		attrs={'UNITS':bx.attrs['Units'],
+		'CATDESC':'Magnetic Field, cartesian'+toCoordSys,
+		'FIELDNAM':"Magnetic field",'AXISLABEL':'B'})
+	vx = h5file['Vx']
+	vy = h5file['Vy']
+	vz = h5file['Vz']
+	if scDic['velCoordSys'] is None:
+		toCoordSys = 'GSM'
+	else:
+		toCoordSys = scDic['velCoordSys']
+	lfmv_out = convertGameraVec(vx[:],vy[:],vz[:],ut,
+		'SM','car',scDic['magCoordSys'],'car')
+	data['GAMERA_Velocity'] = dm.dmarray(lfmv_out.data,
+		attrs={'UNITS':vx.attrs['Units'],
+		'CATDESC':'Velocity, cartesian'+toCoordSys,
+			   'FIELDNAM':"Velocity",'AXISLABEL':'V'})
+	den = h5file['D']
+	data['GAMERA_Density'] = dm.dmarray(den[:],
+		attrs={'UNITS':den.attrs['Units'],
+		'CATDESC':'Density','FIELDNAM':"Density",'AXISLABEL':'n'})
+	pres = h5file['P']
+	data['GAMERA_Pressure'] = dm.dmarray(pres[:],
+		attrs={'UNITS':pres.attrs['Units'],
+		'CATDESC':'Pressure','FIELDNAM':"Pressure",'AXISLABEL':'P'})
+	temp = h5file['T']
+	data['GAMERA_Temperature'] = dm.dmarray(temp[:],
+		attrs={'UNITS':pres.attrs['Units'],
+		'CATDESC':'Temperature','FIELDNAM':"Temperature",
+		'AXISLABEL':'T'})
+
+	if not keep:
+		subprocess.run(['rm',h5name])
+		subprocess.run(['rm',xmlfile])
+		subprocess.run(['rm',fOut])
+	return
+
 
 
 
