609b47a684a0535187da724519b0720d9ec8522e
wiltbemj <wiltbemj@ucar.edu>
2021-11-17 08:07:50 -0700

    Fix bug with setting output coordinate system for velocity

--- kaipy/satcomp/scutils.py
+++ kaipy/satcomp/scutils.py
@@ -402,7 +402,7 @@ def addGAMERA(data,scDic,h5name):
 	else:
 		toCoordSys = scDic['Velocity']['CoordSys']
 	lfmv_out = convertGameraVec(vx[:],vy[:],vz[:],ut,
-		'SM','car',scDic['MagneticField']['CoordSys'],'car')
+		'SM','car',toCoordSys,'car')
 	data['GAMERA_Velocity'] = dm.dmarray(lfmv_out.data,
 		attrs={'UNITS':vx.attrs['Units'],
 		'CATDESC':'Velocity, cartesian'+toCoordSys,
