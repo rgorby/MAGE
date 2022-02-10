aa9ee5fa5d332af58a0d22cf29bced31e9223242
Eric Winter <eric.winter@jhuapl.edu>
2022-02-10 14:57:58 -0500

    Added MPI tiling display.

--- scripts/quicklook/ql_bw3d.py
+++ scripts/quicklook/ql_bw3d.py
@@ -11,14 +11,44 @@ from the first and last steps in the HDF file.
 
 
 import argparse
+import glob
 import os
+from pathlib import Path
+import re
 
 import matplotlib
+from matplotlib import style
 import matplotlib.pyplot as plt
 import numpy as np
 
 import kaipy.kaiH5 as kaih5
 import kaipy.gamera.gampp as gampp
+
+
+def determine_mpi_grid_shape(directory, runid):
+    """Determine the shape of the MPI grid."""
+
+    # Get the path object for the data directory.
+    path = Path(directory)
+
+    # Start with a null shape.
+    nx = None
+    ny = None
+    nz = None
+
+    # Get a list of the files for this runID.
+    pattern =  r"%s_(\d{4})_(\d{4})_(\d{4})_\d{4}_\d{4}_\d{4}.*.h5" % runid
+    regexp = re.compile(pattern)
+    for f in path.iterdir():
+        filename = os.path.split(f)[-1]
+        m = regexp.search(filename)
+        if m:
+            nx = int(m.group(1))
+            ny = int(m.group(2))
+            nz = int(m.group(3))
+            break
+
+    return (nx, ny, nz)
 
 
 if __name__ == "__main__":
@@ -53,6 +83,9 @@ if __name__ == "__main__":
     directory = args.directory
     runid = args.runid
 
+    # Determine the shape of the MPI grid.
+    (n_x, n_y, n_z) = determine_mpi_grid_shape(directory, runid)
+
     # Open a pipe to the data file.
     data_pipe = gampp.GameraPipe(directory, runid)
 
@@ -61,26 +94,27 @@ if __name__ == "__main__":
     kz_0 = np.argmin(np.abs(Z[0, 0, :]))
     z_0 = Z[0, 0, kz_0]
 
-    # Read the grid coordinates.
+    # Read the grid coordinates from the Z=0 layer.
     X = data_pipe.X[..., kz_0]
     Y = data_pipe.Y[..., kz_0]
 
-    # Read the magnetic field components for the first step, and compute
-    # the corresponding magnetic pressure.
-    Bx = data_pipe.GetVar("Bx", data_pipe.s0, doVerb=False)[..., kz_0]
-    By = data_pipe.GetVar("By", data_pipe.s0, doVerb=False)[..., kz_0]
-    Bz = data_pipe.GetVar("Bz", data_pipe.s0, doVerb=False)[..., kz_0]
-    Pb_first = (Bx**2 + By**2 + Bz**2)/2
+    # Determine the X and Y limits.
+    X_min = X.min()
+    X_max = X.max()
+    Y_min = Y.min()
+    Y_max = Y.max()
 
-    # Read the magnetic field components for the last step, and compute
-    # the corresponding magnetic pressure.
-    Bx = data_pipe.GetVar("Bx", data_pipe.sFin, doVerb=False)[..., kz_0]
-    By = data_pipe.GetVar("By", data_pipe.sFin, doVerb=False)[..., kz_0]
-    Bz = data_pipe.GetVar("Bz", data_pipe.sFin, doVerb=False)[..., kz_0]
-    Pb_last = (Bx**2 + By**2 + Bz**2)/2
+    # Compute the MPI tile boundaries assuming they are equally-sized along
+    # each dimension.
+    mpi_tiles_x = np.linspace(X_min, X_max, n_x + 1)
+    mpi_tiles_y = np.linspace(Y_min, Y_max, n_y + 1)
+
+    # Read the pressure at the first and last steps.
+    P_first = data_pipe.GetVar("P", data_pipe.s0, doVerb=False)[..., kz_0]
+    P_last = data_pipe.GetVar("P", data_pipe.sFin, doVerb=False)[..., kz_0]
 
     # Plot parameters
-    name = "Magnetic pressure at z = %s" % z_0
+    name = "Pressure at z = %s" % z_0
     units = "code units"
     vmin = 0
     vmax = 1
@@ -89,26 +123,34 @@ if __name__ == "__main__":
     matplotlib.use("Agg")
     fig, axes = plt.subplots(2, 1)
 
-    # Plot the magnetic pressure from the first step.
+    # Plot the magnetic pressure from the first step, with MPI tiling.
     axes[0].set_aspect("equal")
     axes[0].set_ylabel("Y")
-    values = axes[0].pcolormesh(X, Y, Pb_first, cmap="viridis", vmin=vmin, vmax=vmax)
+    values = axes[0].pcolormesh(X, Y, P_first, cmap="viridis", vmin=vmin, vmax=vmax)
+    for x in mpi_tiles_x[1:-1]:
+        axes[0].axvline(x, linestyle="--", linewidth=0.5, color="0.5")
+    for y in mpi_tiles_y[1:-1]:
+        axes[0].axhline(y, linestyle="--", linewidth=0.5, color="0.5")
     axes[0].text(0.5, 0.4, "Step 0", color="white")
 
-    # Plot the magnetic pressure from the last step.
+    # Plot the magnetic pressure from the last step, with MPI tiling.
     axes[1].set_aspect("equal")
     axes[1].set_xlabel("X")
     axes[1].set_ylabel("Y")
-    values = axes[1].pcolormesh(X, Y, Pb_last, cmap="viridis", vmin=vmin, vmax=vmax)
+    values = axes[1].pcolormesh(X, Y, P_last, cmap="viridis", vmin=vmin, vmax=vmax)
+    for x in mpi_tiles_x[1:-1]:
+        axes[1].axvline(x, linestyle="--", linewidth=0.5, color="0.5")
+    for y in mpi_tiles_y[1:-1]:
+        axes[1].axhline(y, linestyle="--", linewidth=0.5, color="0.5")
     axes[1].text(0.5, 0.4, "Step %s" % data_pipe.sFin, color="white")
 
     # Show the shared colorbar.
     fig.subplots_adjust(right=0.8)
     cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-    fig.colorbar(values, cax=cbar_ax, label="%s [%s]" % ("Magnetic pressure", units))
+    fig.colorbar(values, cax=cbar_ax, label="%s [%s]" % ("Pressure", units))
 
     # Set the plot title.
-    plt.suptitle("Magnetic pressure at start and end for %s" % (runid))
+    plt.suptitle("Pressure at start and end for %s" % (runid))
 
     # Save the quicklook plot.
     figure_file_name = "%s_quicklook.png" % (runid)
