d494119a3bc43b500b0b453b49bbc11dfb4b4318
wiltbemj <wiltbemj@ucar.edu>
2022-03-04 12:09:28 -0700

    Adds functions for creating Kp and Dst from solar wind

--- kaipy/kaiTools.py
+++ kaipy/kaiTools.py
@@ -58,3 +58,55 @@ def xyz2rtp(phi,theta,Ax,Ay,Az):
 	Ap = -Ax*np.sin(phi)+Ay*np.cos(phi)
 	At = Ax*np.cos(phi)*np.cos(theta)+Ay*np.sin(phi)*np.cos(theta)-Az*np.sin(theta)
 	return Ar,At,Ap
+
+# Use the Burton 1975 Formula to compute Dst from solar wind parameters
+def burtonDst(secs,n,vx,vy,vz,bx,by,bz):
+	"""
+	Given time in seconds, density in cm^-3, velocity in km/s and magnetic field in nT 
+	this function will return Dst computed from the Burton 1975 (doi: 10.1029/ja080i031p04204)
+	"""
+	v = np.sqrt(vx**2+vy**2+vz**2)
+	b = np.sqrt(bx**2+by**2+bz**2)
+	ey = 1.0e-3*vx*bz
+	a = 3.6e-5
+	b = 0.2
+	c = 20.0
+	d = -1.5e-3
+	dp = -1.2e-3
+	fe = d*(np.maximum(0,ey-0.5))
+	pdyn = 1.6726e-27*1e6*6.248e18*n*v**2
+	sqrtpdyn = np.sqrt(pdyn)
+	dst = 0*pdyn
+	for i in np.arange(len(dst)-1)+1:
+		dst[i] = dst[i-1] + (secs[i]-secs[i-1])*(fe[i]-a*(dst[i-1]-b*sqrtpdyn[i]+c))
+	return dst
+
+def newellkp(secs,n,vx,vy,vz,bx,by,bz):
+	"""
+	Given time in seconds, density in cm^-3, velocity in km/s and magnetic field in nT 
+	this function will return Kp computed from the Newell 2008 (doi: 10.1029/2007ja012825)
+	"""
+	v = np.sqrt(vx**2+vy**2+vz**2)
+	newellCoup = newellcoupling(vx,vy,vz,bx,by,bz)
+	kp = np.clip(0.05+2.244e-4*newellCoup+2.844e-6*np.sqrt(n)*v**2,0,9.0)
+	# Kp is  three hour index so compute a three hour rolling average using convolve with first and last value extended
+	window = int(3*60*60/(secs[1]-secs[0]))
+	halfwindow = int(window/2)
+	begin = np.ones(halfwindow)
+	end = np.ones(halfwindow)
+	begin[:] = kp[0]
+	end[:] = kp[-1]
+	extendkp = np.concatenate((begin,kp,end))
+	conextendkp = np.convolve(extendkp,np.ones(window)/window,mode='same')
+	return conextendkp[halfwindow:-halfwindow]
+
+def newellcoupling(vx,vy,vz,bx,by,bz):
+	"""
+	Given  density in cm^-3, velocity in km/s and magnetic field in nT 
+	this function will return Universial Coupling Function computed from the Newell 2007 (doi: 10.1029/2006ja012015)
+	"""
+	v = np.sqrt(vx**2+vy**2+vz**2)
+	b = np.sqrt(bx**2+by**2+bz**2)
+	thetac = np.abs(np.arctan2(by,bz))
+	newcoup = np.power(v,4.0/3.0)*np.power(b,2.0/3.0)*np.power(np.sin(thetac/2.0),8.0/3.0)
+	return newcoup
