e6f0ba871addeb126288a1dabecb0ca05fb23cb0
Kareem <kareem.sorathia@gmail.com>
2022-03-22 11:10:13 -0400

    Cleaning up/improving db quick look plot and adding video option

--- scripts/quicklook/dbpic.py
+++ scripts/quicklook/dbpic.py
@@ -13,18 +13,16 @@ import numpy as np
 import kaipy.gamera.gampp as gampp
 import kaipy.kaiH5 as kh5
 import kaipy.cmaps.kaimaps as kmaps
+import kaipy.gamera.deltabViz as dbViz
 import sys
 import os
 import cartopy.crs as ccrs
-from cartopy.feature.nightshade import Nightshade
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
-import matplotlib.ticker as mticker
 
 if __name__ == "__main__":
 	rad2deg = 180.0/np.pi
-	bMag = 1000.0
-	bLin = 10.0
-	jMag = 0.5
+	bMag = dbViz.dbMag
+	bLin = dbViz.dbLin
+	jMag = dbViz.jMag
 
 	#Defaults
 	fdir = os.getcwd()
@@ -60,40 +58,25 @@ if __name__ == "__main__":
 	dbdata = gampp.GameraPipe(fdir,ftag)
 	print("---")
 	#Get coordinates
-	cStr = kh5.PullAtt(fname,"CoordinatesID",s0=None)
-	Re   = kh5.PullAtt(fname,"Re",s0=None) #km
-	try:
-		CoordID = cStr.decode('utf-8')
-	except (UnicodeDecodeError, AttributeError):
-		CoordID = cStr
+	CoordID,Re = dbViz.GetCoords(fname)
 	print("Found %s coordinate data ..."%(CoordID))
+
 	if (nStp<0):
 		nStp = dbdata.sFin
 		print("Using Step %d"%(nStp))
 
-	#Pull data
-	dBn = dbdata.GetVar("dBn",nStp)
-	Rcc = dbdata.GetVar("Radcc",doVerb=False)[:,:,k0]
-	#Get smlat/lon
-	smlon = dbdata.GetVar("smlon",nStp,doVerb=False)[:,:,k0]
-	smlat = dbdata.GetVar("smlat",nStp,doVerb=False)[:,:,k0]
+	#Check vertical level
+	Z0 = dbViz.CheckLevel(dbdata,k0,Re)
 
-	NLat,NLon,Nz = dBn.shape
-	dBn = dBn[:,:,k0]
-	if (k0 >= Nz):
-		print("Invalid vertical level, only %d levels found!"%(Nz))
-		sys.exit()
-	else:
-		Z0 = (Rcc.mean()*Re)-Re
-		print("Height = %6.2f [km]"%(Z0))
+	#Pull data
 	if (doJr):
 		print("Reading Jr ...")
 		Jr = dbdata.GetVar("dbJ",nStp,doVerb=False)[:,:,k0]
 		Q = Jr
 	else:
+		dBn = dbdata.GetVar("dBn",nStp,doVerb=False)[:,:,k0]
 		Q = dBn
 
-	print(Q.min(),Q.max())
 	#Get MJD to UT
 	MJD = kh5.tStep(fname,nStp,aID="MJD")
 	utS = ktools.MJD2UT([MJD])
@@ -102,19 +85,7 @@ if __name__ == "__main__":
 	#=====
 	#Do cartopy stuff
 	crs = ccrs.PlateCarree()
-	
-	X0 = dbdata.X[:,:,k0]
-	Y0 = dbdata.Y[:,:,k0]
-	Z0 = dbdata.Z[:,:,k0]
-	R0 = np.sqrt(X0**2.0 + Y0**2.0 + Z0**2.0)
-	LonI = np.arctan2(Y0,X0)*rad2deg
-	LatI = np.arcsin(Z0/R0) *rad2deg
-
-	LonI = np.linspace(0,360,NLon+1)
-	LatI = LatI[:,0]
-
-	LonC = 0.5*(LonI[0:-1] + LonI[1:])
-	LatC = 0.5*(LatI[0:-1] + LatI[1:])
+	LatI,LonI,LatC,LonC = dbViz.GenUniformLL(dbdata,k0)
 
 	#=====
 	#Do figure stuff
@@ -137,20 +108,7 @@ if __name__ == "__main__":
 	kv.genCB(AxCB,vQ,cbStr,cM=cmap)
 
 	#Add decoration
-	tStr = utDT.strftime("%m/%d/%Y, %H:%M:%S")
-	AxM.set_title(tStr,fontsize="x-large")
-
-	AxM.add_feature(Nightshade(utDT,alpha=0.10))
-	AxM.coastlines(resolution='110m', color='black', linewidth=0.25)
-	
-	gl = AxM.gridlines(crs, draw_labels=True,linewidth=1, color='gray', alpha=0.5, linestyle='--')
-	gl.top_labels = False
-	gl.left_labels = False
-	gl.xlines = False
-	gl.ylines = False
-	gl.xlocator = mticker.FixedLocator([-120, 0, 120])
-	gl.ylocator = mticker.FixedLocator([-45, 0, 45])
-	AxM.contour(LonC,LatC,smlon,[0.5,90,180,270],linewidths=1.25, linestyles='--',alpha=0.5,colors='grey')
-
+	tStr = dbViz.GenTStr(AxM,fname,nStp)
+	dbViz.DecorateDBAxis(AxM,crs,utDT)
 	kv.savePic(fOut)
 	
