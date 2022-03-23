37fdb24822b9e2ef0ca1b67ec3b7b15c1279661f
Kareem <kareem.sorathia@gmail.com>
2022-03-23 09:08:42 -0400

    Adding some comment text to colorbar stuff

--- kaipy/cmaps/kaimaps.py
+++ kaipy/cmaps/kaimaps.py
@@ -4,10 +4,12 @@ import os
 
 #Pull colormap from text file
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
 fIn = os.path.join(__location__,"cmDDiv.txt")
-Q = np.loadtxt(fIn)
+Q = np.loadtxt(fIn,skiprows=1)
 cmDiv = mpl.colors.ListedColormap(Q/255.0)
 
-# Q = np.loadtxt("cmMLT.txt")
-# cmMLT = mpl.colors.ListedColormap(Q/255.0)
+fIn = os.path.join(__location__,"cmMLT.txt")
+Q = np.loadtxt(fIn,skiprows=1)
+cmMLT = mpl.colors.ListedColormap(Q/255.0)
 
