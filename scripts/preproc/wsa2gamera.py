Fixing gravity normalization (still needs checking).
Cleaning up wsa2gamera.py (Still needs cleaning).

Fixing a bug in heliopic.

scripts/preproc/wsa2gamera.py
@@ -12,20 +12,30 @@ import kaipy.gamhelio.lib.wsa as wsa
 
 import kaipy.gamera.gamGrids as gg
 
-#----------- PARSE ARGUMENTS ---------#
+# Parse arguments
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('ConfigFileName',help='The name of the configuration file to use',default='startup.config')
 args = parser.parse_args()
-#----------- PARSE ARGUMENTS ---------#
+
+# constants
+# TODO: move to kaipy/kdefs.py
+# TODO: add Boltzmann constant and remove from pressure calc
+mp = 1.67e-24
 
 # Read params from config file
 prm = params.params(args.ConfigFileName)
 Ng=prm.NO2
 gamma = prm.gamma
+
+# remember to use the same units in gamera
 B0 = prm.B0
 n0 = prm.n0
+
+# TODO move to config file
 T0 = 3.44e6  #2.88e6
+V0 = B0/np.sqrt(4*np.pi*mp*n0)
+# note, keep temperature in K (pressure is normalized in wsa.F90)
 
 #grid parameters
 tMin = prm.tMin
@@ -38,46 +48,27 @@ Nk = prm.Nk
 
 ffits = os.path.join(os.getenv('KAIJUHOME'),prm.wsaFile)
 
-# constants
-mp = 1.67e-24
-
-#----------GENERATE HELIO GRID------
-
+# Generate Helio Grid
 print("Generating gamera-helio grid ...")
 
 X3,Y3,Z3 = gg.GenKSph(Ni=Ni,Nj=Nj,Nk=Nk,Rin=Rin,Rout=Rout,tMin=tMin,tMax=tMax)
-
-#to generate non-uniform grid for GL cme (more fine in region 0.1-0.3 AU) 
-#X3,Y3,Z3 = gg.GenKSphNonUGL(Ni=Ni,Nj=Nj,Nk=Nk,Rin=Rin,Rout=Rout,tMin=tMin,tMax=tMax)
 gg.WriteGrid(X3,Y3,Z3,fOut=os.path.join(prm.GridDir,prm.gameraGridFile))
 
 print("Gamera-helio grid ready!")
 
-#----------GENERATE HELIO GRID------
-
-
-############### WSA STUFF #####################
+# Read/normalize WSA
 jd_c,phi_wsa_v,theta_wsa_v,phi_wsa_c,theta_wsa_c,bi_wsa,v_wsa,n_wsa,T_wsa = wsa.read(ffits,prm.densTempInfile,prm.normalized)
 
-# convert the units; remember to use the same units in gamera
-# TODO: probably store units in the h5 file?
-# B0   = 1.e-3 Gs
-# n0   = 200./cc
-
-V0 = B0/np.sqrt(4*np.pi*mp*n0)
-
+# convert the units
 bi_wsa /= B0
 n_wsa  /= (mp*n0)
 v_wsa /= V0
+
 #convert julian date from wsa fits into modified julian date
 mjd_c = jd_c - 2400000.5
-# keep temperature in K
-############### WSA STUFF #####################
 
 
-############### GAMERA STUFF #####################
-
-# GAMERA GRID
+# GAMERA grid
 with h5py.File(os.path.join(prm.GridDir,prm.gameraGridFile),'r') as f:
     x=f['X'][:]
     y=f['Y'][:]
@@ -108,7 +99,7 @@ Tc = np.arccos(zc[Ng:-Ng,Ng:-Ng,:]/Rc)
 fbi      = interpolate.RectBivariateSpline(phi_wsa_c,theta_wsa_c,bi_wsa.T,kx=1,ky=1)  
 br = fbi(Pc[:,0,0],Tc[0,:,0])
 
-############### SMOOTHING #####################
+# Smoothing
 if not prm.gaussSmoothWidth==0:
     import astropy
     from astropy.convolution import convolve,Gaussian2DKernel
@@ -117,21 +108,18 @@ if not prm.gaussSmoothWidth==0:
     br   =astropy.convolution.convolve(br,gauss,boundary='extend')
 
 
-############### INTERPOLATE AND DUMP #####################
+# Interpolate to Gamera grid
 fv      = interpolate.RectBivariateSpline(phi_wsa_c,theta_wsa_c,v_wsa.T,kx=1,ky=1)  
 vr = fv(Pc[:,0,0],Tc[0,:,0])
 
 f      = interpolate.RectBivariateSpline(phi_wsa_c,theta_wsa_c,n_wsa.T,kx=1,ky=1)  
 rho = f(Pc[:,0,0],Tc[0,:,0])
 
-#f      = interpolate.RectBivariateSpline(phi_wsa_c,theta_wsa_c,T_wsa.T,kx=1,ky=1)  
-#temp = f(Pc[:,0,0],Tc[0,:,0])
+# TODO: clean up, define constants above if needed
+# calculate temperature from total force balance
+# note, this is done AFTER interpolating br and rho to the gamera grid
 temp =  1.*T0/rho + (1.**2-(br)**2)*V0**2 / 2e8/1.38 * 1.67/rho   # *****  
 temp_T = temp.T
-
-pressure =   ((br)**2)*V0**2 /2.*mp*n0 *0.1   + (n0*rho* temp)*1.38e-16 *0.1
-pressure_therm = (n0*rho* temp)*1.38e-16 * 0.1
-pressure_B = (br)**2 *V0**2 / 2.*mp*n0 *0.1
 
 # note, redefining interpolation functions we could also
 # interpolate from bi_wsa as above, but then we would have to
@@ -150,6 +138,7 @@ rho*=(R0/Rc[0,0,:Ng])**2
 br*=(R0/Rc[0,0,:Ng])**2
 br_kface*=(R0/Rc[0,0,:Ng])**2
 
+# Save to file
 with h5py.File(os.path.join(prm.IbcDir,prm.gameraIbcFile),'w') as hf:
     hf.attrs["MJD"] = mjd_c
     hf.create_dataset("vr",data=vr)