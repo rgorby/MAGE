e57673898d1ceb3acc5b917a9e66910a62e68a37
vmerkin <slava.merkin@jhuapl.edu>
2023-04-01 05:35:33 -0600

    Adding min/max grid bounds including ghosts.
    Adding NSEW enumerator.
    
    Fixing indentation.

--- src/base/shellutils/shellGrid.F90
+++ src/base/shellutils/shellGrid.F90
@@ -2,12 +2,18 @@
 module shellGrid
     use kdefs
     use math
-
+    
+    ! note, this will conflict with mixdefs
+    ! but NORTH, SOUTH are defined the same way
+    enum, bind(C)
+        enumerator :: NORTH=1,SOUTH,EAST,WEST
+    end enum
+    
     ! Data type for holding 2D spherical shell grid
     type ShellGrid_T
         integer :: nShellVars = 1 ! number of shell grid functions
         type(shellGridFunction_T), dimension(:), allocatable :: shellVars
-
+        
         integer :: Np,Nt ! Number of lon/lat cells (phi/theta)
         ! xxc = centers (Nx)
         ! xx  = corners (Nx+1)
@@ -18,6 +24,7 @@ module shellGrid
         logical :: doSP = .false., doNP = .false. ! Whether grid contains south/north pole
 
         real(rp) :: minTheta, maxTheta, minPhi, maxPhi
+        real(rp) :: minGTheta, maxGTheta, minGPhi, maxGPhi
 
         ! Local indices, active region
         integer :: is=1,ie,js=1,je
@@ -55,56 +62,56 @@ module shellGrid
 
         ! Parse optional parameters
         if (present(nGhosts)) then
-            shGr%Ngn = nGhosts(1) ! otherwise, always 0 as set in ShellGrid type
-            shGr%Ngs = nGhosts(2) ! otherwise, always 0 as set in ShellGrid type
-            shGr%Nge = nGhosts(3) ! otherwise, always 1 as set in ShellGrid type
-            shGr%Ngw = nGhosts(4) ! otherwise, always 1 as set in ShellGrid type
+            shGr%Ngn = nGhosts(NORTH) ! otherwise, always 0 as set in ShellGrid type
+            shGr%Ngs = nGhosts(SOUTH) ! otherwise, always 0 as set in ShellGrid type
+            shGr%Nge = nGhosts(EAST) ! otherwise, always 1 as set in ShellGrid type
+            shGr%Ngw = nGhosts(WEST) ! otherwise, always 1 as set in ShellGrid type
         end if
 
         ! do some checks first
         if (.not.(isAscending(Theta))) then 
-           write(*,*) "Inside shell grid generator (GenShellGrid)."
-           write(*,*) "Theta array must be ascending. Quitting..."
-           stop
+            write(*,*) "Inside shell grid generator (GenShellGrid)."
+            write(*,*) "Theta array must be ascending. Quitting..."
+            stop
         end if
 
         if (.not.(isAscending(Phi))) then 
-           write(*,*) "Inside shell grid generator (GenShellGrid)."
-           write(*,*) "Phi array must be ascending. Quitting..."
-           stop
+            write(*,*) "Inside shell grid generator (GenShellGrid)."
+            write(*,*) "Phi array must be ascending. Quitting..."
+            stop
         end if
 
         if (any(Theta<0.).or.(any(Theta>PI))) then
-           write(*,*) "Inside shell grid generator (GenShellGrid)."
-           write(*,*) "Theta array should be in the range [0,PI]. Quitting..."
-           stop
+            write(*,*) "Inside shell grid generator (GenShellGrid)."
+            write(*,*) "Theta array should be in the range [0,PI]. Quitting..."
+            stop
         end if
 
         if (any(Phi<0.).or.(any(Phi>2*PI))) then
-           write(*,*) "Inside shell grid generator (GenShellGrid)."
-           write(*,*) "Phi array should be in the range [0,2*PI]. Quitting..."
-           stop
+            write(*,*) "Inside shell grid generator (GenShellGrid)."
+            write(*,*) "Phi array should be in the range [0,2*PI]. Quitting..."
+            stop
         end if
 
         ! decide if the grid is periodic
         if ( ( minval(Phi) <= TINY ).and.( maxval(Phi) >= 2*PI - TINY) ) then
-           shGr%isPeriodic = .True.
+            shGr%isPeriodic = .True.
         else
-           shGr%isPeriodic = .False.
+            shGr%isPeriodic = .False.
         end if
         
         if ( (shGr%isPeriodic).and.(shGr%Nge/=shGr%Ngw) ) then
-           write(*,*) "Inside shell grid generator (GenShellGrid)."
-           write(*,*) "Periodic grid must have the same number of ghosts on low/high phi boundaries. Quitting..."
-           stop
+            write(*,*) "Inside shell grid generator (GenShellGrid)."
+            write(*,*) "Periodic grid must have the same number of ghosts on low/high phi boundaries. Quitting..."
+            stop
         end if
 
         if ( (.not.(shGr%isPeriodic)).and.(shGr%Nge/=shGr%Ngw) ) then
-           write(*,*) "Inside shell grid generator (GenShellGrid)."
-           write(*,*) "Grids with Nge/=Ngw have not been implemented. Quitting..."
-           stop
+            write(*,*) "Inside shell grid generator (GenShellGrid)."
+            write(*,*) "Grids with Nge/=Ngw have not been implemented. Quitting..."
+            stop
         end if
-
+        
         ! Decide if this has north/south pole
         shGr%doNP = .false.
         shGr%doSP = .false.
@@ -117,15 +124,15 @@ module shellGrid
         endif
 
         if ( (shGr%doNP).and.(shGr%Ngn/=0) ) then
-           write(*,*) "Inside shell grid generator (GenShellGrid)."
-           write(*,*) "Grid containing the north pole can't have Ngn/=0. Quitting..."
-           stop
+            write(*,*) "Inside shell grid generator (GenShellGrid)."
+            write(*,*) "Grid containing the north pole can't have Ngn/=0. Quitting..."
+            stop
         end if
 
         if ( (shGr%doSP).and.(shGr%Ngs/=0) ) then
-           write(*,*) "Inside shell grid generator (GenShellGrid)."
-           write(*,*) "Grid containing the south pole can't have Ngs/=0. Quitting..."
-           stop
+            write(*,*) "Inside shell grid generator (GenShellGrid)."
+            write(*,*) "Grid containing the south pole can't have Ngs/=0. Quitting..."
+            stop
         end if
 
         ! define various array indices
@@ -146,14 +153,14 @@ module shellGrid
         allocate(dphi(shGr%Np))   ! helper
         dphi = Phi(2:shGr%Np+1) - Phi(1:shGr%Np)
         if ( all( dphi - dphi(1) <= TINY ) ) then
-           shGr%isPhiUniform = .True.
+            shGr%isPhiUniform = .True.
         else 
-           shGr%isPhiUniform = .False.
+            shGr%isPhiUniform = .False.
         end if
         deallocate(dphi)
 
         associate(is=>shGr%is, ie=>shGr%ie, js=>shGr%js, je=>shGr%je, &
-             isg=>shGr%isg, ieg=>shGr%ieg, jsg=>shGr%jsg, jeg=>shGr%jeg)
+            isg=>shGr%isg, ieg=>shGr%ieg, jsg=>shGr%jsg, jeg=>shGr%jeg)
 
         ! Nuke arrays if already allocated
         if (allocated(shGr%th))   deallocate(shGr%th)
@@ -175,51 +182,51 @@ module shellGrid
 
         ! Do north, unless it's the pole (no ghosts for poles)
         if ( (.not.shGr%doNP).and.(shGr%Ngn/=0) ) then
-           ! linearly extrapolate
-           do i=1,shGr%Ngn
-              shGr%th(is-i) = 2*shGr%th(is) - shGr%th(is+i)
-           end do
+            ! linearly extrapolate
+            do i=1,shGr%Ngn
+                shGr%th(is-i) = 2*shGr%th(is) - shGr%th(is+i)
+            end do
 
-           ! check if we got into negative thetas
-           ! then just fill the space with same size cells down to theta=0
-           if ( shGr%th(is-shGr%Ngn) < 0 ) then
-              delta = shGr%th(is)/shGr%Ngn
+            ! check if we got into negative thetas
+            ! then just fill the space with same size cells down to theta=0
+            if ( shGr%th(is-shGr%Ngn) < 0 ) then
+                delta = shGr%th(is)/shGr%Ngn
 
-              do i=1,shGr%Ngn
-                 shGr%th(is-i) = shGr%th(is) - delta*i
-              end do
-           end if
+                do i=1,shGr%Ngn
+                    shGr%th(is-i) = shGr%th(is) - delta*i
+                end do
+            end if
         end if
 
         ! Do south, unless it's the pole (no ghosts for poles)
         if ( (.not.shGr%doSP).and.(shGr%Ngs/=0) ) then
-           ! linearly extrapolate
-           do i=1,shGr%Ngs
-              shGr%th(ie+1+i) = 2*shGr%th(ie+1) - shGr%th(ie+1-i)
-           end do
+            ! linearly extrapolate
+            do i=1,shGr%Ngs
+                shGr%th(ie+1+i) = 2*shGr%th(ie+1) - shGr%th(ie+1-i)
+            end do
 
-           ! check if we got into thetas > pi
-           ! then just fill the space with same size cells up to theta=pi
-           if ( shGr%th(ie+1+shGr%Ngs) > PI ) then
-              delta = (PI - shGr%th(ie+1))/shGr%Ngs
-              do i=1,shGr%Ngs
-                 shGr%th(ie+1+i) = shGr%th(ie+1) + delta*i
-              end do
-           end if
+            ! check if we got into thetas > pi
+            ! then just fill the space with same size cells up to theta=pi
+            if ( shGr%th(ie+1+shGr%Ngs) > PI ) then
+                delta = (PI - shGr%th(ie+1))/shGr%Ngs
+                do i=1,shGr%Ngs
+                    shGr%th(ie+1+i) = shGr%th(ie+1) + delta*i
+                end do
+            end if
         end if
 
         ! if non-periodic grid is needed, can implement ghosts on E/W ends similarly to N/S above
         ! but we assume the grid is always periodic
         if ( (.not.shGr%isPeriodic) )  then
-           write(*,*) "Inside shell grid generator (GenShellGrid)."
-           write(*,*) "Non-periodic grids are not implemented. Quitting..."
-           stop
+            write(*,*) "Inside shell grid generator (GenShellGrid)."
+            write(*,*) "Non-periodic grids are not implemented. Quitting..."
+            stop
         endif
 
         ! Note, we already made sure that for a periodic grid Nge=Ngw, phi(js)=0 and phi(je+1)=pi
         do j=1,shGr%Nge
-           shGr%ph(js-j)   = shGr%ph(je+1-j) - 2*PI
-           shGr%ph(je+1+j) = shGr%ph(js+j) + 2*PI
+            shGr%ph(js-j)   = shGr%ph(je+1-j) - 2*PI
+            shGr%ph(je+1+j) = shGr%ph(js+j) + 2*PI
         end do
 
         ! Set centers
@@ -231,6 +238,12 @@ module shellGrid
         shGr%maxTheta = maxval(shGr%th(is:ie+1))
         shGr%minPhi   = minval(shGr%ph(js:je+1))
         shGr%maxPhi   = maxval(shGr%ph(js:je+1))
+
+        ! this includes ghosts
+        shGr%minGTheta = minval(shGr%th)
+        shGr%maxGTheta = maxval(shGr%th)
+        shGr%minGPhi   = minval(shGr%ph)
+        shGr%maxGPhi   = maxval(shGr%ph)
 
         end associate
 
@@ -245,10 +258,10 @@ module shellGrid
         contains
             subroutine initShellGridFunctions(shellVar)
                 type(shellGridFunction_T), intent(out) :: shellVar
-
+                
                 if (.not.allocated(shellVar%cellData)) allocate(shellVar%cellData(shGr%Nt,shGr%Np))
                 shellVar%cellData = 0.  ! initialize to 0
-
+                
                 ! unset all BC's
                 shellVar%bcsApplied = .false.
                 
