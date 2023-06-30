cdf0cb4ab14db8a0a9dc9d5bb46b511becb1aae0
Jeffrey Garretson <Jeffrey.Garretson@jhuapl.edu>
2023-06-30 10:13:28 -0600

    Adding error checking to pin cpu script, correcting error auto calculating physical cores

--- scripts/preproc/pinCpuCores.sh
+++ scripts/preproc/pinCpuCores.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-# usage: pinCpuCores.sh prog <args>
+# usage: pinCpuCores.sh <program executable> <program arguments>
 
 # THIS IS ONLY MEANT TO WORK ON DERECHO
 # USE ON OTHER SYSTEMS AT YOUR OWN RISK
 
-let numCores=`lscpu | sed --quiet "s/^CPU(s): \\+\\([0-9]\\+\\)$/\\1/p"`
+if [[ ! -v PMI_LOCAL_SIZE ]] || [[ ! -v PMI_LOCAL_RANK ]]; then
+    echo "pinCpuCores.sh is only meant to be used on systems that set the"
+    echo "   PMI_LOCAL_SIZE and PMI_LOCAL_RANK environment variables."
+    echo "   One or both of these was not set. Please use the"
+    echo "   correctOMPEnvironment.sh script in conjunction with omplace instead"
+    exit 1
+fi
+
+let numCpus=`lscpu | sed --quiet "s/^CPU(s): \\+\\([0-9]\\+\\)$/\\1/p"`
+let threadsPerCore=`lscpu | sed --quiet "s/^Thread(s) per core: \\+\\([0-9]\\+\\)$/\\1/p"`
+let numCores=$numCpus/$threadsPerCore
 let newNumThreads=$numCores/$PMI_LOCAL_SIZE
 let minThread=$newNumThreads*$PMI_LOCAL_RANK
 let maxThread=$minThread+$newNumThreads-1
