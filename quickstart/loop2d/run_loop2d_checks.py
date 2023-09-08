11396aff6b9b3523a3d8cf1fb2a36e97fa1a6143
Eric Winter <eric.winter@jhuapl.edu>
2023-09-08 08:25:55 -0700

    Updated PBS template. Cleaned up run_checks script.

--- quickstart/loop2d/run_loop2d_checks.py
+++ quickstart/loop2d/run_loop2d_checks.py
@@ -42,29 +42,34 @@ def create_command_line_parser():
     -------
     parser : argparse.ArgumentParser
         Command-line argument parser for this script.
+
+    Raises
+    ------
+    None
     """
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument(
-        "-d", "--debug", action="store_true", default=False,
+        "--debug", "-d", action="store_true", default=False,
         help="Print debugging output (default: %(default)s)."
     )
     parser.add_argument(
         "--directory", type=str, metavar="directory", default=os.getcwd(),
-        help="Directory to contain files generated for the run (default: %(default)s)"
+        help="Directory to contain files generated for the run "
+             "(default: %(default)s)"
     )
     parser.add_argument(
         "--runid", type=str, metavar="runid", default=default_runid,
         help="ID string of the run (default: %(default)s)"
     )
     parser.add_argument(
-        "-v", "--verbose", action="store_true", default=False,
+        "--verbose", "-v", action="store_true", default=False,
         help="Print verbose output (default: %(default)s)."
     )
     return parser
 
 
 def compute_volume_integrated_magnetic_pressure(directory, runid):
-    """Compute the volume-integrated magnetic pressure at start and endxs.
+    """Compute the volume-integrated magnetic pressure at start and end.
 
     Compute the volume-integrated magnetic pressure for the first and
     last steps.
@@ -106,9 +111,8 @@ def compute_volume_integrated_magnetic_pressure(directory, runid):
     return Pb_integrated_first, Pb_integrated_last
 
 
-if __name__ == "__main__":
+def main():
     """Begin main program."""
-
     # Set up the command-line parser.
     parser = create_command_line_parser()
 
@@ -118,12 +122,18 @@ if __name__ == "__main__":
     directory = args.directory
     runid = args.runid
     verbose = args.verbose
+    if debug:
+        print(f"args = {args}")
 
     # Compute the volume-integrated magnetic pressure.
     if verbose:
         print("Computing volume-integrated magnetic pressure.")
     PbV1, PbV2 = compute_volume_integrated_magnetic_pressure(directory, runid)
-    if verbose:
-        print("Volume-integrated magnetic pressure (SUM(Pb*dV), code units):")
-        print("At start: %s" % PbV1)
-        print("At end: %s" % PbV2)
+    print("Volume-integrated magnetic pressure (SUM(Pb*dV), code units):")
+    print(f"At start: {PbV1}")
+    print(f"At end: {PbV2}")
+
+
+if __name__ == "__main__":
+    """Begin main program."""
+    main()
