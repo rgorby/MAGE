afb69a43b53f61f389640c65cfc21fceebbcc9e3
Anthony <asciola93@gmail.com>
2024-01-17 14:28:57 -0700

    string-compatable fish

--- src/base/git_info.F90
+++ src/base/git_info.F90
@@ -1,5 +1,5 @@
 module git_info
     implicit none
-    character(len=*), parameter :: gitB = '><((('>'
-    character(len=*), parameter :: gitH = '><((('>'
+    character(len=*), parameter :: gitB = '  ><>  ><>'
+    character(len=*), parameter :: gitH = '<><  <><  '
 end module git_info
