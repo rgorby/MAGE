e09b745678d68c474ae5ae501e0e488ef7737390
Anthony <asciola93@gmail.com>
2024-03-08 17:18:32 -0700

    WIP: paraview ePhi calc

--- kaipy/paraview/pvGam.py
+++ kaipy/paraview/pvGam.py
@@ -1,5 +1,19 @@
 import paraview.simple as pvs
 
+
+# Helper
+def makePF(base, scriptStr, filterName):
+	pf = pvs.ProgrammableFilter(Input=base)
+	pf.Script = scriptStr
+	pf.RequestInformationScript = ''
+	pf.RequestUpdateExtentScript = ''
+	pf.PythonPath = ''
+	pf.CopyArrays=1
+	pvs.RenameSource(scriptStr, pf)
+	# Show
+	renderView1 = pvs.GetActiveViewOrCreate('RenderView')
+	programmableFilter2Display = pvs.Show(pf, renderView1, 'GeometryRepresentation')
+
 def pf_calcSpeeds(base):
 
 	scriptStr = """
@@ -25,14 +39,54 @@ output.CellData.append(va,'Va')
 output.CellData.append(msmach,'Magnetosonic Mach')
 
 """
+	makePF(base, scriptStr, 'pf_calcSpeeds')
 
-	pf = pvs.ProgrammableFilter(Input=base)
-	pf.Script = scriptStr
-	pf.RequestInformationScript = ''
-	pf.RequestUpdateExtentScript = ''
-	pf.PythonPath = ''
-	pf.CopyArrays=1
-	pvs.RenameSource('pf_calcSpeeds', pf)
-	# Show
-	renderView1 = pvs.GetActiveViewOrCreate('RenderView')
-	programmableFilter2Display = pvs.Show(pf, renderView1, 'GeometryRepresentation')
+
+def pf_calcEfield(base):
+
+	scriptStr="""
+import kaipy.kdefs as kdefs
+import kaipy.kaiTools as kt
+in0 = inputs[0]
+
+Bx = in0.CellData['Bx']
+By = in0.CellData['By']
+Bz = in0.CellData['Bz']
+Vx = in0.CellData['Vx']
+Vy = in0.CellData['Vy']
+Vz = in0.CellData['Vz']
+
+#E=-VxB
+Ex = -(Vy*Bz-Vz*By)*0.001 # [mV/m]
+Ey =  (Vx*Bz-Vz*Bx)*0.001
+Ez = -(Vx*By-Vy*Bx)*0.001
+
+xxx
+
+output.CellData.append(Ex,'Ex')
+output.CellData.append(Ey,'Ey')
+output.CellData.append(Ez,'Ez')
+"""
+	makePF(base, scriptStr, 'pf_Efield')
+	
+#------
+
+"""
+Bx = gsph.EggSlice("Bx",nStp,doEq=True)
+	By = gsph.EggSlice("By",nStp,doEq=True)
+	Bz = gsph.EggSlice("Bz",nStp,doEq=True)
+	Vx = gsph.EggSlice("Vx",nStp,doEq=True)
+	Vy = gsph.EggSlice("Vy",nStp,doEq=True)
+	Vz = gsph.EggSlice("Vz",nStp,doEq=True)
+
+	# calculating some variables to to plot
+	#E=-VxB
+	Ex = -(Vy*Bz-Vz*By)*0.001 # [mV/m]
+	Ey =  (Vx*Bz-Vz*Bx)*0.001
+	Ez = -(Vx*By-Vy*Bx)*0.001
+
+	# coordinate transform
+	ppc = np.arctan2(gsph.yyc,gsph.xxc)
+	theta = np.pi #eq plane
+	Er,Et,Ep = kt.xyz2rtp(ppc,theta,Ex,Ey,Ez)
+"""
