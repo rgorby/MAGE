3677cb03921ea57026259d1fe50da0f2fc34b793
Eric Winter <eric.winter@jhuapl.edu>
2024-04-16 10:45:43 -0600

    Switched to compact datetime format for directory names.

--- testingScripts/buildTest.py
+++ testingScripts/buildTest.py
@@ -326,50 +326,52 @@ def main():
 
     # -------------------------------------------------------------------------
 
-    # Set up for communication with Slack.
-    # slack_client = common.slack_create_client()
-    # if debug:
-    #     print(f"slack_client = {slack_client}")
+    Set up for communication with Slack.
+    slack_client = common.slack_create_client()
+    if debug:
+        print(f"slack_client = {slack_client}")
 
-    # # -------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
 
-    # # Detail the test results
-    # test_details_message = ''
-    # for (i_test, module_list_file) in enumerate(module_list_files):
-    #     module_set_name = module_list_file.rstrip('.lst')
-    #     test_details_message += f"Module set `{module_set_name}`: "
-    #     if test_passed[i_test]:
-    #         test_details_message += '*PASSED*\n'
-    #     else:
-    #         test_details_message += '*FAILED*\n'
+    # Detail the test results
+    test_report_details_string = ''
+    for (i_test, module_list_file) in enumerate(module_list_files):
+        module_set_name = module_list_file.rstrip('.lst')
+        test_report_details_string += f"Module set `{module_set_name}`: "
+        if test_passed[i_test]:
+            test_report_details_string += '*PASSED*\n'
+        else:
+            test_report_details_string += '*FAILED*\n'
 
-    # # Summarize the test results.
-    # test_summary_message = (
-    #     'Summary of build test results from `buildTest.py`: '
-    # )
-    # if 'FAILED' in test_details_message:
-    #     test_summary_message += '*FAILED*\n'
-    # else:
-    #     test_summary_message += '*ALL PASSED*\n'
+    # Summarize the test results.
+    test_report_summary_string = (
+        'Summary of build test results from `buildTest.py`: '
+    )
+    if 'FAILED' in test_report_details_string:
+        test_report_summary_string += '*FAILED*\n'
+    else:
+        test_report_summary_string += '*ALL PASSED*\n'
 
-    # # Print the test results summary and details.
-    # print(test_summary_message)
-    # print(test_details_message)
+    # Print the test results summary and details.
+    print(test_report_summary_string)
+    print(test_report_details_string)
 
-    # # If loud mode is on, post report to Slack.
-    # if be_loud:
-    #     test_summary_message += 'Details in thread for this messsage.\n'
-    #     slack_response_summary = common.slack_send_message(
-    #         slack_client, test_summary_message, is_test=is_test
-    #     )
-    #     if slack_response_summary['ok']:
-    #         thread_ts = slack_response_summary['ts']
-    #         slack_response_details = common.slack_send_message(
-    #             slack_client, test_details_message, thread_ts=thread_ts,
-    #             is_test=is_test
-    #         )
-    #     else:
-    #         print('*ERROR* Unable to post test result summary to Slack.')
+    # If loud mode is on, post report to Slack.
+    if be_loud:
+        test_report_summary_string += 'Details in thread for this messsage.\n'
+        slack_response_summary = common.slack_send_message(
+            slack_client, test_report_summary_string, is_test=is_test
+        )
+        if slack_response_summary['ok']:
+            thread_ts = slack_response_summary['ts']
+            slack_response_details = common.slack_send_message(
+                slack_client, test_report_details_string, thread_ts=thread_ts,
+                is_test=is_test
+            )
+            if not 'ok' in slack_response_summary:
+                print('*ERROR* Unable to post test details to Slack.')
+        else:
+            print('*ERROR* Unable to post test summary to Slack.')
 
     # -------------------------------------------------------------------------
 
