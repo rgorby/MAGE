5cd192170357c64d40ac84474563ee713037f131
Jeffrey Garretson <Jeffrey.Garretson@jhuapl.edu>
2024-04-18 12:04:51 -0600

    Not reverting floating point changes

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -83,13 +83,9 @@ if(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
 	#Base
 	string(APPEND CMAKE_Fortran_FLAGS " -fPIC -fpconstant")
 	#Production
-    if(NOT FAST_YOLO)
-        set(PROD "-align array64byte -align rec32byte -fimf-use-svml=true -fp-model=source")
-    else()
-        set(PROD "-align array64byte -align rec32byte -fimf-use-svml=true -fp-model=fast")
-    endif()
+    set(PROD "-align array64byte -align rec32byte -no-prec-div -fast-transcendentals")
     #Production with Debug Info
-    set(PRODWITHDEBUGINFO "-traceback -debug all -align array64byte -align rec32byte -fimf-use-svml=true -fp-model=source")
+    set(PRODWITHDEBUGINFO "-traceback -debug all -align array64byte -align rec32byte -no-prec-div -fast-transcendentals")
 	#Debug
     if(NOT DISABLE_DEBUG_BOUNDS_CHECKS)
         set(DEBUG "-traceback -check bounds -check uninit -debug all -gen-interfaces -warn interfaces -fp-stack-check")
