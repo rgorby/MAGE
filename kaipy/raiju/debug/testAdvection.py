fb8664acb5998736aad64024c70454ba40998779
Anthony <asciola93@gmail.com>
2024-04-23 14:47:59 -0600

    different vid options

--- kaipy/raiju/debug/testAdvection.py
+++ kaipy/raiju/debug/testAdvection.py
@@ -3,6 +3,7 @@ import h5py as h5
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import cmasher as cm
 
 import kaipy.kaiH5 as kh5
 import kaipy.kdefs as kd
@@ -412,6 +413,345 @@ def makeVid(raijuInfo, outdir, stride=1):
         Ax_eta_eHigh.clear()
 
 
+
+def makeVid2(raijuInfo, outdir, stride=1):
+
+    fontsize_title=6
+    vidOut = 'raijuVid2'
+    kh5.CheckDirOrMake(os.path.join(outdir, vidOut))
+
+    fig = plt.figure(figsize=(20,10))
+    cmW = 0.2
+    gs = gridspec.GridSpec(2, 8, width_ratios = [1, cmW, 1, cmW, 1, cmW, 1, cmW], height_ratios=[1, 1], hspace=0.2)
+
+    # Top row: drivers
+    #  MHD bVol | MHD den | MHD press | None
+
+    # Botom row: RAIJU
+    # Psph | Mid H+ | Mid e- | total Pressure
+
+    norm_den = kv.genNorm(0.1, 1000, doLog=True)
+    norm_press = kv.genNorm(0, 70, doLog=False)
+    norm_bVol = kv.genNorm(3e-3, 0.3, doLog=True)
+    norm_eta = kv.genNorm(1e-1,1e10, doLog=True)
+    cm_den = 'viridis'
+    cm_press = 'plasma'
+    cm_bVol = cm.prinsenvlag
+    cm_eta = cm.voltage
+
+    axBvol  = fig.add_subplot(gs[0,0])
+    axDen   = fig.add_subplot(gs[0,2])    
+    axPress = fig.add_subplot(gs[0,4])
+    axCM_bvol  = fig.add_subplot(gs[0,1])
+    axCM_den   = fig.add_subplot(gs[0,3])
+    axCM_press = fig.add_subplot(gs[0,5])
+
+    axPsph   = fig.add_subplot(gs[1,0])
+    axMidH   = fig.add_subplot(gs[1,2])
+    axMidE   = fig.add_subplot(gs[1,4])
+    axRPress = fig.add_subplot(gs[1,6])
+    #axCM_psph   = fig.add_subplot(gs[1,1])
+    #axCM_midH   = fig.add_subplot(gs[3,1])
+    #axCM_midE   = fig.add_subplot(gs[5,1])
+    axCM_eta   = fig.add_subplot(gs[1,1])
+    #axCM_RPress = fig.add_subplot(gs[7,1])
+
+    kv.genCB(axCM_bvol , norm_bVol , 'Flux tube volume', cm_bVol, doVert=True)
+    kv.genCB(axCM_den  , norm_den  , 'Density [#/cc]', cm_den, doVert=True)
+    kv.genCB(axCM_press, norm_press, 'Pressure [nPa]', cm_press, doVert=True)
+    
+    kv.genCB(axCM_eta, norm_eta, 'eta', cm_eta, doVert=True)
+
+    xBnds = [-15, 10]
+
+    f5 = h5.File(raijuInfo.fname, 'r')
+    alamc = f5['alamc'][:]
+    spc_psph = raijuInfo.species[ru.flavs_s['PSPH']]
+    k_psph = spc_psph.kStart
+    spc_hotp = raijuInfo.species[ru.flavs_s['HOTP']]
+    k_pMid  = spc_hotp.kStart + int(spc_hotp.N//2)
+    spc_hote = raijuInfo.species[ru.flavs_s['HOTE']]
+    k_eMid  = spc_hote.kStart + int(spc_hote.N//2)
+
+    n_pad = int(np.log10(raijuInfo.Nt)) + 1
+    nplt = 0
+    for n in raijuInfo.steps[::stride]:
+        filename = "vid.{:0>{npad}d}.png".format(nplt, npad=n_pad)
+        filename = os.path.join(outdir, vidOut, filename)
+        nplt += 1
+        if os.path.exists(filename):
+            continue
+        if (np.mod(nplt,5) == 0):
+            print("\tvid %d"%(n))
+
+        s5 = f5[raijuInfo.stepStrs[n]]
+        time = s5.attrs['time']
+        xmin = ru.getVar(s5, 'xmin')
+        ymin = ru.getVar(s5, 'ymin')
+        active = ru.getVar(s5, 'active')
+        topo = ru.getVar(s5, 'topo')
+        eta  = ru.getVar(s5, 'eta' )
+        bVol = ru.getVar(s5, 'bVol')
+        denIn = ru.getVar(s5,'Davg_in')[:,:,2]
+        pressIn = ru.getVar(s5,'Pavg_in')[:,:,2]
+        pressOut = ru.getVar(s5,'Pressure')[:,:,2]
+
+        bVol = kt.to_center2D(bVol)
+
+        active3d = np.broadcast_to(active[:,:,np.newaxis], eta.shape)
+        eta      = np.ma.masked_where(active3d != ru.domain["ACTIVE"], eta     )
+        #bVol    = np.ma.masked_where(topo != ru.topo['CLOSED'], bVol   )
+        bVol = np.ma.masked_where(active != ru.domain['ACTIVE'], bVol)
+        denIn   = np.ma.masked_where(active != ru.domain['ACTIVE'], denIn  )
+        pressIn = np.ma.masked_where(active != ru.domain['ACTIVE'], pressIn)
+        pressOut = np.ma.masked_where(active != ru.domain["ACTIVE"], pressOut)
+
+        energies = alamc[np.newaxis,np.newaxis,:] * bVol[:,:,np.newaxis]**(-2/3) * 1e-3  # [keV]
+        energies = np.abs(energies)
+
+        # energy eval loc
+        evalLoc = [-4, 0]
+        distSq = (xmin-evalLoc[0])**2 + (ymin-evalLoc[1])**2  # Each slice point's distance from particle
+        aMinFlattened = distSq.argmin()  # Index of min distance, if it was a flattened array
+        i, j = np.unravel_index(aMinFlattened, xmin.shape)  # Convert back into i,j location
+        eEval_H = energies[i, j, k_pMid]
+        eEval_E = energies[i, j, k_eMid]
+
+        # MHD bVol
+        ru.plotXYMin(axBvol , xmin, ymin, bVol, norm=norm_bVol, cmap=cm_bVol, shading='auto', lblsize=None)
+        kv.setBndsByAspect(axBvol, xBnds)
+        # MHD den
+        ru.plotXYMin(axDen , xmin, ymin, denIn, norm=norm_den, cmap=cm_den, shading='auto', lblsize=None)
+        kv.setBndsByAspect(axDen, xBnds)
+        # MHD press
+        ru.plotXYMin(axPress , xmin, ymin, pressIn, norm=norm_press, cmap=cm_press, shading='auto', lblsize=None)
+        kv.setBndsByAspect(axPress, xBnds)
+
+        # RAIJU psph
+        ru.plotXYMin(axPsph , xmin, ymin, eta[:,:,k_psph ], norm=norm_eta, cmap=cm_eta, shading='auto', lblsize=None)
+        axPsph.set_title('Plasmasphere', fontsize=fontsize_title)
+        kv.setBndsByAspect(axPsph, xBnds)
+        # RAIJU mid H+
+        ru.plotXYMin(axMidH , xmin, ymin, eta[:,:,k_pMid ], norm=norm_eta, cmap=cm_eta, shading='auto', lblsize=None)
+        axMidH.set_title('H+ $\lambda$={}, {:0.2f} keV @ {} R$_E$'.format(k_pMid, eEval_H, evalLoc[0]), fontsize=fontsize_title)
+        kv.setBndsByAspect(axMidH, xBnds)
+        # RAIJU mid e-
+        ru.plotXYMin(axMidE , xmin, ymin, eta[:,:,k_eMid ], norm=norm_eta, cmap=cm_eta, shading='auto', lblsize=None)
+        axMidE.set_title('e- $\lambda$={}, {:0.2f} keV @ {} R$_E$'.format(k_eMid, eEval_E, evalLoc[0]), fontsize=fontsize_title)
+        kv.setBndsByAspect(axMidE, xBnds)
+        # RAIJU total Pressure
+        ru.plotXYMin(axRPress , xmin, ymin, pressOut, norm=norm_press, cmap=cm_press, shading='auto', lblsize=None)
+        axRPress.set_title('Total pressure', fontsize=fontsize_title)
+        kv.setBndsByAspect(axRPress, xBnds)
+
+        axBvol.xaxis.set_major_formatter(plt.NullFormatter())
+        axDen.xaxis.set_major_formatter(plt.NullFormatter())
+        axPress.xaxis.set_major_formatter(plt.NullFormatter())
+        axBvol.yaxis.set_major_formatter(plt.NullFormatter())
+        axDen.yaxis.set_major_formatter(plt.NullFormatter())
+
+        axPsph.yaxis.set_major_formatter(plt.NullFormatter())
+        axMidH.yaxis.set_major_formatter(plt.NullFormatter())
+        axMidE.yaxis.set_major_formatter(plt.NullFormatter())
+
+
+        fig.suptitle("step# {}; t = {:0.2f} s".format(n, time))
+        kv.savePic(filename)
+
+        axBvol.clear()
+        axDen.clear()
+        axPress .clear()
+        axPsph.clear()
+        axMidH .clear()
+        axMidE .clear()
+        axRPress.clear()
+
+
+def makeVid3(raijuInfo, outdir, stride=1):
+
+    fontsize_title=18
+    vidOut = 'raijuVid3'
+    kh5.CheckDirOrMake(os.path.join(outdir, vidOut))
+
+    fig = plt.figure(figsize=(24,10))
+    cmW = 0.1
+    gs = gridspec.GridSpec(4, 3, 
+                           width_ratios = [1, 1, 1], 
+                           height_ratios=[cmW, 1, 1, cmW], 
+                           hspace=0.12, wspace=0.1)
+
+    # Top row: drivers
+    # MHD den | MHD press | MHD bVol
+
+    # Botom row: RAIJU
+    # Psph | Mid H+ | Mid e-
+
+    norm_den = kv.genNorm(0.1, 1e4, doLog=True)
+    norm_press = kv.genNorm(0, 50, doLog=False)
+    norm_bVol = kv.genNorm(3e-3, 0.3, doLog=True)
+    norm_psph = kv.genNorm(1e-1,1e10, doLog=True)
+    norm_midh = kv.genNorm(5e2,1e8, doLog=True)
+    norm_mide = kv.genNorm(5e2,1e8, doLog=True)
+    cm_den = 'viridis'
+    cm_press = 'plasma'
+    cm_bVol = cm.prinsenvlag
+    cm_eta = cm.voltage
+
+    axCM_den   = fig.add_subplot(gs[0,0])
+    axCM_press = fig.add_subplot(gs[0,1])
+    axCM_bvol  = fig.add_subplot(gs[0,2])
+    axDen   = fig.add_subplot(gs[1,0])    
+    axPress = fig.add_subplot(gs[1,1])
+    axBvol  = fig.add_subplot(gs[1,2])
+    
+    axPsph   = fig.add_subplot(gs[2,0])
+    axMidH   = fig.add_subplot(gs[2,1])
+    axMidE   = fig.add_subplot(gs[2,2])
+    axCM_Psph   = fig.add_subplot(gs[3,0])
+    axCM_MidH   = fig.add_subplot(gs[3,1])
+    axCM_MidE   = fig.add_subplot(gs[3,2])
+
+    kv.genCB(axCM_bvol , norm_bVol , 'Flux tube volume', cm_bVol , doVert=False, cbSz='large')
+    kv.genCB(axCM_den  , norm_den  , 'Density [#/cc]'  , cm_den  , doVert=False, cbSz='large')
+    kv.genCB(axCM_press, norm_press, 'Pressure [nPa]'  , cm_press, doVert=False, cbSz='large')
+    axCM_bvol .xaxis.set_ticks_position('top')
+    axCM_den  .xaxis.set_ticks_position('top')
+    axCM_press.xaxis.set_ticks_position('top')
+    axCM_bvol .xaxis.set_label_position('top')
+    axCM_den  .xaxis.set_label_position('top')
+    axCM_press.xaxis.set_label_position('top')
+
+    
+    kv.genCB(axCM_Psph, norm_psph, 'eta', cm_eta, doVert=False, cbSz='large')
+    kv.genCB(axCM_MidH, norm_midh, 'eta', cm_eta, doVert=False, cbSz='large')
+    kv.genCB(axCM_MidE, norm_mide, 'eta', cm_eta, doVert=False, cbSz='large')
+
+    xBnds = [-15, 10]
+
+    f5 = h5.File(raijuInfo.fname, 'r')
+    alamc = f5['alamc'][:]
+    spc_psph = raijuInfo.species[ru.flavs_s['PSPH']]
+    k_psph = spc_psph.kStart
+    spc_hotp = raijuInfo.species[ru.flavs_s['HOTP']]
+    k_pMid  = spc_hotp.kStart + int(spc_hotp.N//4)
+    spc_hote = raijuInfo.species[ru.flavs_s['HOTE']]
+    k_eMid  = spc_hote.kStart + int(spc_hote.N//3)
+
+    n_pad = int(np.log10(raijuInfo.Nt)) + 1
+    nplt = 0
+    for n in raijuInfo.steps[::stride]:
+        filename = "vid.{:0>{npad}d}.png".format(nplt, npad=n_pad)
+        filename = os.path.join(outdir, vidOut, filename)
+        nplt += 1
+        if os.path.exists(filename):
+            continue
+        if (np.mod(nplt,5) == 0):
+            print("\tvid %d"%(n))
+
+        s5 = f5[raijuInfo.stepStrs[n]]
+        time = s5.attrs['time']
+        xmin = ru.getVar(s5, 'xmin')[4:,:]
+        ymin = ru.getVar(s5, 'ymin')[4:,:]
+        active = ru.getVar(s5, 'active')[4:,:]
+        topo = ru.getVar(s5, 'topo')[4:,:]
+        eta  = ru.getVar(s5, 'eta' )[4:,:]
+        bVol = ru.getVar(s5, 'bVol')[4:,:]
+        denIn = ru.getVar(s5,'Davg_in')[4:,:,2]
+        pressIn = ru.getVar(s5,'Pavg_in')[4:,:,2]
+        pressOut = ru.getVar(s5,'Pressure')[4:,:,2]
+
+        bVol = kt.to_center2D(bVol)
+
+        active3d = np.broadcast_to(active[:,:,np.newaxis], eta.shape)
+        eta      = np.ma.masked_where(active3d != ru.domain["ACTIVE"], eta     )
+        #bVol    = np.ma.masked_where(topo != ru.topo['CLOSED'], bVol   )
+        bVol = np.ma.masked_where(active != ru.domain['ACTIVE'], bVol)
+        denIn   = np.ma.masked_where(active != ru.domain['ACTIVE'], denIn  )
+        pressIn = np.ma.masked_where(active != ru.domain['ACTIVE'], pressIn)
+        pressOut = np.ma.masked_where(active != ru.domain["ACTIVE"], pressOut)
+
+        energies = alamc[np.newaxis,np.newaxis,:] * bVol[:,:,np.newaxis]**(-2/3) * 1e-3  # [keV]
+        energies = np.abs(energies)
+
+        # energy eval loc
+        evalLoc = [-4, 0]
+        distSq = (xmin-evalLoc[0])**2 + (ymin-evalLoc[1])**2  # Each slice point's distance from particle
+        aMinFlattened = distSq.argmin()  # Index of min distance, if it was a flattened array
+        i, j = np.unravel_index(aMinFlattened, xmin.shape)  # Convert back into i,j location
+        eEval_H = energies[i, j, k_pMid]
+        eEval_E = energies[i, j, k_eMid]
+
+        # MHD den
+        ru.plotXYMin(axDen , xmin, ymin, denIn, norm=norm_den, cmap=cm_den, shading='auto', lblsize=None)
+        kv.setBndsByAspect(axDen, xBnds)
+        kv.addEarth2D(ax=axDen)
+        # MHD press
+        ru.plotXYMin(axPress , xmin, ymin, pressIn, norm=norm_press, cmap=cm_press, shading='auto', lblsize=None)
+        kv.setBndsByAspect(axPress, xBnds)
+        kv.addEarth2D(ax=axPress)
+        # MHD bVol
+        ru.plotXYMin(axBvol , xmin, ymin, bVol, norm=norm_bVol, cmap=cm_bVol, shading='auto', lblsize=None)
+        kv.setBndsByAspect(axBvol, xBnds)
+        kv.addEarth2D(ax=axBvol)
+
+        textLoc = [0.5, 0.1]
+        textSize = 14
+        # RAIJU psph
+        ru.plotXYMin(axPsph , xmin, ymin, eta[:,:,k_psph ], norm=norm_psph, cmap=cm_eta, shading='auto', lblsize=None)
+        t = axPsph.text(textLoc[0], textLoc[1],'Plasmasphere', \
+                horizontalalignment='center', verticalalignment='top', \
+                transform=axPsph.transAxes,fontsize=textSize)
+        t.set_bbox(dict(facecolor='white',edgecolor='black',alpha=0.85))
+        kv.setBndsByAspect(axPsph, xBnds)
+        kv.addEarth2D(ax=axPsph)
+        # RAIJU mid H+
+        ru.plotXYMin(axMidH , xmin, ymin, eta[:,:,k_pMid ], norm=norm_midh, cmap=cm_eta, shading='auto', lblsize=None)
+        t = axMidH.text(textLoc[0], textLoc[1],'H+, {:0.2f} keV @ {} R$_E$'.format(eEval_H, np.abs(evalLoc[0])), \
+                horizontalalignment='center', verticalalignment='top', \
+                transform=axMidH.transAxes,fontsize=textSize)
+        t.set_bbox(dict(facecolor='white',edgecolor='black',alpha=0.85))
+        kv.setBndsByAspect(axMidH, xBnds)
+        kv.addEarth2D(ax=axMidH)
+        # RAIJU mid e-
+        ru.plotXYMin(axMidE , xmin, ymin, eta[:,:,k_eMid ], norm=norm_mide, cmap=cm_eta, shading='auto', lblsize=None)
+        t = axMidE.text(textLoc[0], textLoc[1],'e-, {:0.2f} keV @ {} R$_E$'.format(eEval_E, np.abs(evalLoc[0])), \
+                horizontalalignment='center', verticalalignment='top', \
+                transform=axMidE.transAxes,fontsize=textSize)
+        t.set_bbox(dict(facecolor='white',edgecolor='black',alpha=0.85))
+        kv.setBndsByAspect(axMidE, xBnds)
+        kv.addEarth2D(ax=axMidE)
+
+        #axDen  .xaxis.set_major_formatter(plt.NullFormatter())
+        #axPress.xaxis.set_major_formatter(plt.NullFormatter())
+        #axBvol .xaxis.set_major_formatter(plt.NullFormatter())
+        axDen  .tick_params(axis='x', which='major', pad=-0.9)
+        axPress.tick_params(axis='x', which='major', pad=-0.9)
+        axBvol .tick_params(axis='x', which='major', pad=-0.9)
+        axDen  .yaxis.set_major_formatter(plt.NullFormatter())
+        axPress.yaxis.set_major_formatter(plt.NullFormatter())
+
+        axPsph.xaxis.set_major_formatter(plt.NullFormatter())
+        axMidH.xaxis.set_major_formatter(plt.NullFormatter())
+        axMidE.xaxis.set_major_formatter(plt.NullFormatter())
+        axPsph.xaxis.set_ticks_position('top')
+        axMidH.xaxis.set_ticks_position('top')
+        axMidE.xaxis.set_ticks_position('top')
+        axPsph.yaxis.set_major_formatter(plt.NullFormatter())
+        axMidH.yaxis.set_major_formatter(plt.NullFormatter())
+
+
+        fig.suptitle("step# {}; t = {:0.2f} s".format(n, time))
+        kv.savePic(filename)
+
+        axBvol.clear()
+        axDen.clear()
+        axPress .clear()
+        axPsph.clear()
+        axMidH .clear()
+        axMidE .clear()
+
+
+
 if __name__=="__main__":
     # Open file
     f5name = os.path.join(dir, raih5fname)
@@ -463,4 +803,6 @@ if __name__=="__main__":
 
     # plotStep(raijuInfo, 0, 10)
 
-    makeVid(raijuInfo, dir)
+    #makeVid(raijuInfo, dir)
+    #makeVid2(raijuInfo, dir, 5)
+    makeVid3(raijuInfo, dir, 1)
