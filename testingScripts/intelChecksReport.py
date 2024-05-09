3b37f3dd5ab5a6e473407fff263c635b663e3f27
Eric Winter <eric.winter@jhuapl.edu>
2024-05-09 13:09:23 -0600

    More linting.

--- testingScripts/intelChecksReport.py
+++ testingScripts/intelChecksReport.py
@@ -84,89 +84,87 @@ def main():
         print(f"job2 = {job2}")
 
     # Combine the job output files.
-    jobFile1 = f"memCheck.o{job1}"
-    jobFile2 = f"threadCheck.o{job2}"
+    job_file_1 = f"memCheck.o{job1}"
+    job_file_2 = f"threadCheck.o{job2}"
     if debug:
-        print(f"jobFile1 = {jobFile1}")
-        print(f"jobFile2 = {jobFile2}")
+        print(f"job_file_1 = {job_file_1}")
+        print(f"job_file_2 = {job_file_2}")
 
     # Check the the logs exist.
-    if not os.path.exists(jobFile1):
-        print(f"{jobFile1} not found, tests are incomplete.")
+    if not os.path.exists(job_file_1):
+        print(f"{job_file_1} not found, tests are incomplete.")
         sys.exit(0)
-    if not os.path.exists(jobFile2):
-        print(f"{jobFile2} not found, tests are incomplete.")
+    if not os.path.exists(job_file_2):
+        print(f"{job_file_2} not found, tests are incomplete.")
         sys.exit(0)
 
     # Set up regex for counting problems found.
-    problemPattern = '(\d+) new problem\(s\) found'
+    problem_pattern = r'(\d+) new problem\(s\) found'
 
     # ------------------------------------------------------------------------
 
     # Search the output logs for errors.
 
     # Memory
-    memErrs = False
-    memErrsFile = 'combinedMemErrs.txt'
-    for root, dirs, files in os.walk('.'):
+    memory_errors_file = 'combinedMemErrs.txt'
+    for _, dirs, _ in os.walk('.'):
         for d in dirs:
             if 'memResults' in d:
                 try:
-                    memOut = subprocess.check_output(
+                    memory_check_output = subprocess.check_output(
                         ['inspxe-cl', '-report summary', '-result-dir ' + d,
                          '-s-f memSuppress.sup'],
                         stderr=subprocess.STDOUT, universal_newlines=True)
-                except subprocess.CalledProcessError as memProcErr:
+                except subprocess.CalledProcessError as e:
                     # we need to handle non-zero error code
-                    memOut = memProcErr.output
-                problemMatch = re.search(problemPattern, memOut)
-                if not problemMatch or int(problemMatch.group(1)) > 0:
-                    memErrs = True
+                    memory_check_output = e.output
+                problem_match = re.search(problem_pattern, memory_check_output)
+                if not problem_match or int(problem_match.group(1)) > 0:
                     try:
-                        memOut = subprocess.check_output(
+                        memory_check_output = subprocess.check_output(
                             ['inspxe-cl', '-report problems',
                              '-result-dir ' + d, '-s-f memSuppress.sup',
                              '-report-all'],
                             stderr=subprocess.STDOUT, universal_newlines=True)
-                    except subprocess.CalledProcessError as memProcErr:
+                    except subprocess.CalledProcessError as e:
                         # we need to handle non-zero error code
-                        memOut = memProcErr.output
-                    with open(memErrsFile, 'a', encoding='utf-8') as memFile:
-                        memFile.write(memOut)
-                        memFile.write('\n')
+                        memory_check_output = e.output
+                    with open(
+                        memory_errors_file, 'a', encoding='utf-8'
+                    ) as f:
+                        f.write(memory_check_output)
+                        f.write('\n')
 
     # Thread
-    threadErrs = False
-    threadErrsFile = 'combinedThreadErrs.txt'
-    for root, dirs, files in os.walk('.'):
+    thread_errors_file = 'combinedThreadErrs.txt'
+    for _, dirs, _ in os.walk('.'):
         for d in dirs:
             if 'threadResults' in d:
                 try:
-                    threadOut = subprocess.check_output(
+                    thread_check_output = subprocess.check_output(
                         ['inspxe-cl', '-report summary', '-result-dir ' + d,
                          '-s-f threadSuppress.sup'],
                         stderr=subprocess.STDOUT, universal_newlines=True
                     )
-                except subprocess.CalledProcessError as threadProcErr:
+                except subprocess.CalledProcessError as e:
                     # we need to handle non-zero error code
-                    threadOut = threadProcErr.output
-                problemMatch = re.search(problemPattern, threadOut)
-                if not problemMatch or int(problemMatch.group(1)) > 0:
-                    threadErrs = True
+                    thread_check_output = e.output
+                problem_match = re.search(problem_pattern, thread_check_output)
+                if not problem_match or int(problem_match.group(1)) > 0:
                     try:
-                        threadOut = subprocess.check_output([
+                        thread_check_output = subprocess.check_output([
                             'inspxe-cl', '-report problems',
                             '-result-dir ' + d, '-s-f threadSuppress.sup',
                             '-report-all'],
                             stderr=subprocess.STDOUT, universal_newlines=True)
-                    except subprocess.CalledProcessError as threadProcErr:
+                    except subprocess.CalledProcessError as e:
                         # we need to handle non-zero error code
-                        threadOut = threadProcErr.output
+                        thread_check_output = e.output
                     with open(
-                        threadErrsFile, 'a', encoding='utf-8'
-                    ) as threadFile:
-                        threadFile.write(threadOut)
-                        threadFile.write('\n')
+                        thread_errors_file, 'a', encoding='utf-8'
+                    ) as f:
+                        f.write(thread_check_output)
+                        f.write('\n')
 
     # -----------------------------------------------------------------------
 
@@ -180,11 +178,11 @@ def main():
     # Detail the test results
     test_report_details_string = ''
     test_report_details_string += 'Results of memory tests:\n'
-    with open(memErrsFile, 'r', encoding='utf-8') as f:
+    with open(memory_errors_file, 'r', encoding='utf-8') as f:
         test_report_details_string += f.readlines()
     test_report_details_string += '\n'
     test_report_details_string += 'Results of thread tests:\n'
-    with open(threadErrsFile, 'r', encoding='utf-8') as f:
+    with open(thread_errors_file, 'r', encoding='utf-8') as f:
         test_report_details_string += f.readlines()
 
     # Summarize the test results
