8599646394d76a66f0c2e64f2eddb443a2f81d15
Eric Winter <eric.winter@jhuapl.edu>
2024-05-24 11:56:20 -0400

    Replaced docs with original wiki home page.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MAGE'
+copyright = '2024, Center for Geospace Storms'
+author = 'Center for Geospace Storms'
 
 release = '0.1'
 version = '0.1.0'
