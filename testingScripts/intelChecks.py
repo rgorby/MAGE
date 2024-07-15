11efe2c0d3c827e192da5a5c59b76dd4dd685c27
Eric Winter <eric.winter@jhuapl.edu>
2024-07-15 06:36:29 -0600

    Fixed Inspector reporting errors.

--- testingScripts/intelChecks.py
+++ testingScripts/intelChecks.py
@@ -29,7 +29,7 @@ import subprocess
 from jinja2 import Template
 
 # Import project modules.
-from kaipy.testing import common
+import common
 
 
 # Program constants
@@ -56,15 +56,6 @@ MODULE_LIST_DIRECTORY = os.path.join(TEST_SCRIPTS_DIRECTORY,
 # Name of file containing names of modules lists to use for Intel checks
 INTEL_CHECKS_LIST_FILE = os.path.join(MODULE_LIST_DIRECTORY, 'intelChecks.lst')
 
-# Prefix for naming Intel Inspector checks directories
-INTEL_CHECKS_DIRECTORY_PREFIX = 'intelChecks_'
-
-# Name of build subdirectory containing binaries
-BUILD_BIN_DIR = 'bin'
-
-# Name of PBS account to use for testing jobs.
-DERECHO_TESTING_ACCOUNT = os.environ['DERECHO_TESTING_ACCOUNT']
-
 # Path to jinja2 template file for PBS script for the memory tests.
 MEM_CHECK_PBS_TEMPLATE = os.path.join(
     TEST_SCRIPTS_DIRECTORY, 'intelCheckSubmitMem-template.pbs'
@@ -80,14 +71,30 @@ REPORT_PBS_TEMPLATE = os.path.join(
     TEST_SCRIPTS_DIRECTORY, 'intelCheckSubmitReport-template.pbs'
 )
 
+# Prefix for naming Intel Inspector checks directories
+INTEL_CHECKS_DIRECTORY_PREFIX = 'intelChecks_'
+
+# Name of build subdirectory containing binaries
+BUILD_BIN_DIR = 'bin'
+
 # Name of PBS file for memory checks.
 MEM_CHECK_PBS_FILENAME = 'intelCheckSubmitMem.pbs'
 
 # Name of PBS file for thread checks.
 THREAD_CHECK_PBS_FILENAME = 'intelCheckSubmitThread.pbs'
 
-# Name of PBS file for report.
+# Name of PBS file for report generation.
 REPORT_PBS_FILENAME = 'intelCheckSubmitReport.pbs'
+
+# Data and configuration files used by the Intel Inspector tests.
+TEST_INPUT_FILES = [
+    'tinyCase.xml',
+    'bcwind.h5',
+    'lfmD.h5',
+    'rcmconfig.h5',
+    'memSuppress.sup',
+    'threadSuppress.sup',
+]
 
 
 def main():
@@ -115,9 +122,9 @@ def main():
     args = parser.parse_args()
     if args.debug:
         print(f"args = {args}")
-    # account = args.account
     debug = args.debug
     be_loud = args.loud
+    slack_on_fail = args.slack_on_fail
     is_test = args.test
     verbose = args.verbose
 
@@ -177,8 +184,20 @@ def main():
     if debug:
         print(f"make_cmd = {make_cmd}")
 
+    # Create the list for submit results. Only set to True if all qsub commands
+    # for a set are OK.
+    submit_ok = [False]*len(module_list_files)
+    if debug:
+        print(f"submit_ok = {submit_ok}")
+
+    # Create a list of lists for job IDs. There are 3 job IDs per set - one for
+    # memory check, one for thread check, and one for the test report.
+    job_ids = [[None, None, None]]*len(module_list_files)
+    if debug:
+        print(f"job_ids = {job_ids}")
+
     # Run Intel checks with each set of modules.
-    for module_list_file in module_list_files:
+    for (i_module_set, module_list_file) in enumerate(module_list_files):
         if verbose:
             print('Performing Intel Inspector checks with module set '
                   f"{module_list_file}.")
@@ -201,18 +220,18 @@ def main():
             print(f"cmake_environment = {cmake_environment}")
             print(f"cmake_options = {cmake_options}")
 
+        # Add the additional flags needed for Intel Inspector checks.
+        cmake_options += ' -DDISABLE_DEBUG_BOUNDS_CHECKS=ON'
+        cmake_options += ' -DCMAKE_BUILD_TYPE=DEBUG'
+        if debug:
+            print(f"cmake_options = {cmake_options}")
+
         # Assemble the commands to load the listed modules.
         module_cmd = (
             f"module --force purge; module load {' '.join(module_names)}"
         )
         if debug:
             print(f"module_cmd = {module_cmd}")
-
-        # Add the additional flags needed for Intel Inspector checks.
-        cmake_options += ' -DDISABLE_DEBUG_BOUNDS_CHECKS=ON'
-        cmake_options += ' -DCMAKE_BUILD_TYPE=DEBUG'
-        if debug:
-            print(f"cmake_options = {cmake_options}")
 
         # Make a directory for this test, and go there.
         dir_name = f"{INTEL_CHECKS_DIRECTORY_PREFIX}{module_set_name}"
@@ -224,37 +243,30 @@ def main():
 
         # Run cmake to build the Makefile.
         if verbose:
-            print(
-                'Running cmake to create Makefile for module set'
-                f" {module_set_name}."
-            )
-        cmd = (
-            f"{module_cmd}; {cmake_environment} cmake {cmake_options}"
-            f" {KAIJUHOME} >& cmake.out"
-        )
+            print('Running cmake to create Makefile for module set'
+                  f" {module_set_name}.")
+        cmd = (f"{module_cmd}; {cmake_environment} cmake {cmake_options}"
+               f" {KAIJUHOME} >& cmake.out")
         if debug:
             print(f"cmd = {cmd}")
         try:
             # NOTE: stdout and stderr goes cmake.out.
-            _ = subprocess.run(cmd, shell=True, check=True)
+            cproc = subprocess.run(cmd, shell=True, check=True)
         except subprocess.CalledProcessError as e:
-            print(
-                f"ERROR: cmake for module set {module_set_name} failed.\n"
-                f"e.cmd = {e.cmd}\n"
-                f"e.returncode = {e.returncode}\n"
-                f"See {os.path.join(build_directory, 'cmake.out')}"
-                ' for output from cmake.\n'
-                f"Skipping remaining steps for module set {module_set_name}.",
-                file=sys.stderr
-            )
+            print(f"ERROR: cmake for module set {module_set_name} failed.\n"
+                  f"e.cmd = {e.cmd}\n"
+                  f"e.returncode = {e.returncode}\n"
+                  f"See {os.path.join(build_directory, 'cmake.out')}"
+                  ' for output from cmake.\n'
+                  'Skipping remaining steps for module set '
+                  f"{module_set_name}.",
+                  file=sys.stderr)
             continue
 
         # Run the build.
         if verbose:
-            print(
-                'Running make to build kaiju for module set'
-                f" {module_set_name}."
-            )
+            print('Running make to build kaiju for module set'
+                  f" {module_set_name}.")
         cmd = f"{module_cmd}; {make_cmd} >& make.out"
         if debug:
             print(f"cmd = {cmd}")
@@ -262,37 +274,28 @@ def main():
             # NOTE: stdout and stderr go into make.out.
             cproc = subprocess.run(cmd, shell=True, check=True)
         except subprocess.CalledProcessError as e:
-            print(
-                f"ERROR: make for module set {module_set_name} failed.\n"
-                f"e.cmd = {e.cmd}\n"
-                f"e.returncode = {e.returncode}\n"
-                f"See {os.path.join(build_directory, 'make.out')}"
-                ' for output from make.\n'
-                f"Skipping remaining steps for module set {module_set_name}.",
-                file=sys.stderr
-            )
+            print(f"ERROR: make for module set {module_set_name} failed.\n"
+                  f"e.cmd = {e.cmd}\n"
+                  f"e.returncode = {e.returncode}\n"
+                  f"See {os.path.join(build_directory, 'make.out')}"
+                  ' for output from make.\n'
+                  'Skipping remaining steps for module set '
+                  f"{module_set_name}.",
+                  file=sys.stderr)
             continue
 
         # Go to the bin directory for testing.
         os.chdir(BUILD_BIN_DIR)
 
         # Copy in the files used by the tests.
-        test_files = [
-            'tinyCase.xml',
-            'bcwind.h5',
-            'lfmD.h5',
-            'rcmconfig.h5',
-            'memSuppress.sup',
-            'threadSuppress.sup',
-        ]
-        for filename in test_files:
+        for filename in TEST_INPUT_FILES:
             from_path = os.path.join(TEST_SCRIPTS_DIRECTORY, filename)
             to_path = os.path.join('.', filename)
             shutil.copyfile(from_path, to_path)
 
         # Assemble common data to fill in the PBS templates.
         pbs_options = {}
-        pbs_options['account'] = DERECHO_TESTING_ACCOUNT
+        pbs_options['account'] = os.environ['DERECHO_TESTING_ACCOUNT']
         pbs_options['queue'] = os.environ['DERECHO_TESTING_QUEUE']
         pbs_options['job_priority'] = os.environ['DERECHO_TESTING_PRIORITY']
         pbs_options['modules'] = module_names
@@ -300,7 +303,6 @@ def main():
         pbs_options['tmpdir'] = os.environ['TMPDIR']
         pbs_options['slack_bot_token'] = os.environ['SLACK_BOT_TOKEN']
         pbs_options['mage_test_root'] = os.environ['MAGE_TEST_ROOT']
-        pbs_options['derecho_testing_account'] = os.environ['DERECHO_TESTING_ACCOUNT']
 
         # Set options specific to the memory check, then render the template.
         pbs_options['job_name'] = 'mage_intelCheckSubmitMem'
@@ -320,12 +322,20 @@ def main():
         # template.
         pbs_options['job_name'] = 'mage_intelCheckReportSubmit'
         pbs_options['walltime'] = '02:00:00'
+        pbs_options['report_options'] = ''
+        if debug:
+            pbs_options['report_options'] += ' -d'
+        if be_loud:
+            pbs_options['report_options'] += ' -l'
+        if slack_on_fail:
+            pbs_options['report_options'] += ' -s'
+        if is_test:
+            pbs_options['report_options'] += ' -t'
+        if verbose:
+            pbs_options['report_options'] += ' -v'
         pbs_content = report_pbs_template.render(pbs_options)
         with open(REPORT_PBS_FILENAME, 'w', encoding='utf-8') as f:
             f.write(pbs_content)
-
-        # Initialize the job ID list.
-        job_ids = []
 
         # Run the memory check job.
         cmd = f"qsub {MEM_CHECK_PBS_FILENAME}"
@@ -335,19 +345,18 @@ def main():
             cproc = subprocess.run(cmd, shell=True, check=True,
                                    text=True, capture_output=True)
         except subprocess.CalledProcessError as e:
-            print(
-                'ERROR: qsub failed.\n'
-                f"e.cmd = {e.cmd}\n"
-                f"e.returncode = {e.returncode}\n"
-                'See test log for output.\n'
-                f"Skipping remaining steps for module set {module_set_name}.",
-                file=sys.stderr
-            )
+            print('ERROR: qsub failed.\n'
+                  f"e.cmd = {e.cmd}\n"
+                  f"e.returncode = {e.returncode}\n"
+                  'See test log for output.\n'
+                  'Skipping remaining steps for module set '
+                  f"{module_set_name}.",
+                  file=sys.stderr)
             continue
         job_id = cproc.stdout.split('.')[0]
         if debug:
             print(f"job_id = {job_id}")
-        job_ids.append(job_id)
+        job_ids[i_module_set][0] = job_id
 
         # Run the thread check job.
         cmd = f"qsub {THREAD_CHECK_PBS_FILENAME}"
@@ -357,68 +366,77 @@ def main():
             cproc = subprocess.run(cmd, shell=True, check=True,
                                    text=True, capture_output=True)
         except subprocess.CalledProcessError as e:
-            print(
-                'ERROR: qsub failed.\n'
-                f"e.cmd = {e.cmd}\n"
-                f"e.returncode = {e.returncode}\n"
-                'See test log for output.\n'
-                f"Skipping remaining steps for module set {module_set_name}.",
-                file=sys.stderr
-            )
+            print('ERROR: qsub failed.\n'
+                  f"e.cmd = {e.cmd}\n"
+                  f"e.returncode = {e.returncode}\n"
+                  'See test log for output.\n'
+                  'Skipping remaining steps for module set '
+                  f"{module_set_name}.",
+                  file=sys.stderr)
             continue
         job_id = cproc.stdout.split('.')[0]
         if debug:
             print(f"job_id = {job_id}")
-        job_ids.append(job_id)
+        job_ids[i_module_set][1] = job_id
 
         # Run the report job when the other two jobs are complete.
-        cmd = f"qsub -W depend=afterok:{':'.join(job_ids)} {REPORT_PBS_FILENAME}"
+        cmd = (
+            f"qsub -W depend=afterok:{':'.join(job_ids[i_module_set][:-1])} "
+            f"{REPORT_PBS_FILENAME}"
+        )
         if debug:
             print(f"cmd = {cmd}")
         try:
             cproc = subprocess.run(cmd, shell=True, check=True,
                                    text=True, capture_output=True)
         except subprocess.CalledProcessError as e:
-            print(
-                'ERROR: qsub failed.\n'
-                f"e.cmd = {e.cmd}\n"
-                f"e.returncode = {e.returncode}\n"
-                'See test log for output.\n'
-                f"Skipping remaining steps for module set {module_set_name}.",
-                file=sys.stderr
-            )
+            print('ERROR: qsub failed.\n'
+                  f"e.cmd = {e.cmd}\n"
+                  f"e.returncode = {e.returncode}\n"
+                  'See test log for output.\n'
+                  'Skipping remaining steps for module set '
+                  f"{module_set_name}.",
+                  file=sys.stderr)
             continue
         job_id = cproc.stdout.split('.')[0]
         if debug:
             print(f"job_id = {job_id}")
-        job_ids.append(job_id)
+        job_ids[i_module_set][2] = job_id
 
-        # Record the job IDs.
+        # Record the job IDs for this module set in a file.
         with open('jobs.txt', 'w', encoding='utf-8') as f:
-            for job_id in job_ids:
+            for job_id in job_ids[i_module_set]:
                 f.write(f"{job_id}\n")
 
+        # This module set worked.
+        submit_ok[i_module_set] = True
+
+    # End of loop over module sets.
+
     # -----------------------------------------------------------------------
-
-    # Set up for communication with Slack.
-    slack_client = common.slack_create_client()
-    if debug:
-        print(f"slack_client = {slack_client}")
-
-    # ------------------------------------------------------------------------
 
     # Detail the test results
     test_report_details_string = ''
     test_report_details_string += (
-        f"Test results are in {os.getcwd()}.\n"
+        f"Test results are on `derecho` in `{INTEL_CHECKS_DIRECTORY}`.\n"
     )
-    pbs_files = [
-        MEM_CHECK_PBS_FILENAME, THREAD_CHECK_PBS_FILENAME,
-        REPORT_PBS_FILENAME
-    ]
-    for (pbs_file, job_id) in zip(pbs_files, job_ids):
+    for (i_module_set, module_list_file) in enumerate(module_list_files):
+        if not submit_ok[i_module_set]:
+            test_report_details_string += (
+                f"Module set `{module_list_file}`: *FAILED*"
+            )
+            continue
         test_report_details_string += (
-            f"{pbs_file} submitted as job {job_id}.\n"
+            f"`{MEM_CHECK_PBS_FILENAME}` for module set `{module_list_file}` "
+            f"submitted as PBS job {job_ids[i_module_set][0]}.\n"
+        )
+        test_report_details_string += (
+            f"`{THREAD_CHECK_PBS_FILENAME}` for module set `{module_list_file}` "
+            f"submitted as PBS job {job_ids[i_module_set][1]}.\n"
+        )
+        test_report_details_string += (
+            f"`{REPORT_PBS_FILENAME}` for module set `{module_list_file}` "
+            f"submitted as PBS job {job_ids[i_module_set][2]}.\n"
         )
 
     # Summarize the test results
@@ -430,22 +448,23 @@ def main():
     print(test_report_summary_string)
     print(test_report_details_string)
 
-    # If loud mode is on, post report to Slack.
-    if be_loud:
-        test_report_summary_string += 'Details in thread for this messsage.\n'
+    # If a test failed, or loud mode is on, post report to Slack.
+    if (slack_on_fail and 'FAILED' in test_report_details_string) or be_loud:
+        slack_client = common.slack_create_client()
+        if debug:
+            print(f"slack_client = {slack_client}")
         slack_response_summary = common.slack_send_message(
             slack_client, test_report_summary_string, is_test=is_test
         )
-        if slack_response_summary['ok']:
-            thread_ts = slack_response_summary['ts']
-            slack_response_details = common.slack_send_message(
-                slack_client, test_report_details_string, thread_ts=thread_ts,
-                is_test=is_test
-            )
-            if 'ok' not in slack_response_details:
-                print('*ERROR* Unable to post test details to Slack.')
-        else:
-            print('*ERROR* Unable to post test summary to Slack.')
+        if debug:
+            print(f"slack_response_summary = {slack_response_summary}")
+        thread_ts = slack_response_summary['ts']
+        slack_response_summary = common.slack_send_message(
+            slack_client, test_report_details_string, thread_ts=thread_ts,
+            is_test=is_test
+        )
+        if debug:
+            print(f"slack_response_summary = {slack_response_summary}")
 
     # ------------------------------------------------------------------------
 
